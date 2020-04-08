1095f8709d3a7ad3eef56d93b8e9043ab1e7b737
Baku Hashimoto <mail@baku89.com>
2020-04-09 08:23:15 +0900

    Change the draw call representation by using symbols

--- src/impl/view.ts
+++ src/impl/view.ts
@@ -11,58 +11,71 @@ replEnv.set('$insert', (item: MalVal) => {
 	return null
 })
 
+const _SYM = Symbol.for
+
 function draw(ctx: CanvasRenderingContext2D, ast: MalVal) {
 	if (Array.isArray(ast)) {
 		const [cmd, ...args] = ast as any[]
 
 		const last = args.length > 0 ? args[args.length - 1] : null
 
-		if (cmd === 'fill') {
-			ctx.beginPath()
+		if (cmd === _SYM('fill')) {
 			draw(ctx, last)
 			ctx.fillStyle = args[0] as string
 			ctx.fill()
-		} else if (cmd === 'stroke') {
-			ctx.beginPath()
+		} else if (cmd === _SYM('stroke')) {
 			draw(ctx, last)
 			ctx.strokeStyle = args[0]
 			ctx.lineWidth = args[1]
 			ctx.stroke()
-		} else if (cmd === 'rect') {
-			;(ctx.rect as any)(...args)
-		} else if (cmd === 'circle') {
-			ctx.arc(args[0], args[1], args[2], 0, 2 * Math.PI)
-		} else if (cmd === 'line') {
-			const [x0, y0, x1, y1] = args as number[]
-			ctx.moveTo(x0, y0)
-			ctx.lineTo(x1, y1)
-		} else if (cmd === 'path') {
-			for (const [c, a0, a1] of args) {
-				switch (c) {
-					case 'M':
-						ctx.moveTo(a0, a1)
+		} else if (cmd === _SYM('path')) {
+			ctx.beginPath()
+			for (let i = 0; i < args.length; i++) {
+				switch (args[i]) {
+					case _SYM('M'):
+						ctx.moveTo(args[++i], args[++i])
 						break
-					case 'L':
-						ctx.lineTo(a0, a1)
+					case _SYM('L'):
+						ctx.lineTo(args[++i], args[++i])
 						break
-					case 'Z':
+					case _SYM('A'):
+						ctx.arc(
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i]
+						)
+						break
+					case _SYM('C'):
+						ctx.bezierCurveTo(
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i],
+							args[++i]
+						)
+						break
+					case _SYM('Z'):
 						ctx.closePath()
 						break
 					default:
-						throw new Error(`Invalid d-path command: ${c}`)
+						throw new Error(`Invalid d-path command: ${args[i]}`)
 				}
 			}
-		} else if (cmd === 'translate') {
+		} else if (cmd === _SYM('translate')) {
 			ctx.save()
 			ctx.translate(args[0] as number, args[1] as number)
 			draw(ctx, last)
 			ctx.restore()
-		} else if (cmd === 'scale') {
+		} else if (cmd === _SYM('scale')) {
 			ctx.save()
 			ctx.scale(args[0] as number, args[1] as number)
 			draw(ctx, last)
 			ctx.restore()
-		} else if (cmd === 'rotate') {
+		} else if (cmd === _SYM('rotate')) {
 			ctx.save()
 			ctx.rotate(args[0] as number)
 			draw(ctx, last)
