e167abed7b1c30e4ed2c0e321fd43d82e73ac504
Baku Hashimoto <mail@baku89.com>
2020-04-30 15:45:39 +0900

    Implement hashmap destruction in fn and let

--- src/mal/env.ts
+++ src/mal/env.ts
@@ -1,4 +1,11 @@
-import {MalVal, LispError, symbolFor as S, isSymbol} from './types'
+import {
+	MalVal,
+	LispError,
+	symbolFor as S,
+	isSymbol,
+	isMap,
+	MalMap
+} from './types'
 
 type Binds = (string | Binds)[]
 
@@ -32,12 +39,14 @@ export default class Env {
 			this.data[binds] = exprs
 		} else {
 			for (let i = 0; i < binds.length; i++) {
+				// Variable length arguments
 				if (binds[i] === S('&')) {
-					// variable length arguments
 					this.data[binds[i + 1] as string] = exprs.slice(i)
 					break
 				}
+
 				if (Array.isArray(binds[i])) {
+					// List Destruction
 					if (Array.isArray(exprs[i])) {
 						this.bindAll(binds[i] as Binds, exprs[i] as MalVal[])
 					} else {
@@ -45,6 +54,36 @@ export default class Env {
 							`Error: destruction parameter ['${(binds[i] as string[])
 								.map(s => s.slice(1))
 								.join(' ')}'] is not specified as list`
+						)
+					}
+				} else if (isMap(binds[i])) {
+					// Hashmap destruction
+					// binds: {name :name location :location} <-- exprs: {:name "Baku" :location "Japan"}
+					if (isMap(exprs[i])) {
+						// Convert the two maps to list
+						// binds: [name location] <-- exprs: ["Baku" "Japan"]
+						const entries = Object.entries(binds[i]) as [string, string][],
+							hashBinds = [],
+							hashExprs = []
+
+						for (const [sym, key] of entries) {
+							if (!(key in (exprs[i] as MalMap))) {
+								throw new LispError(
+									`ERROR: destruction keyword :${key.slice(
+										1
+									)} does not exist on the parameter`
+								)
+							}
+							hashBinds.push(sym)
+							hashExprs.push((exprs[i] as MalMap)[key])
+						}
+
+						this.bindAll(hashBinds, hashExprs)
+					} else {
+						throw new LispError(
+							`Error: destruction parameter {'${(binds[i] as string[])
+								.map(s => s.slice(1))
+								.join(' ')}'} is not specified as map`
 						)
 					}
 				} else if (exprs[i] === undefined) {
