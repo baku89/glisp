1b6707efa6af14aef7f16d685d715ed2abdd5fbe
Baku Hashimoto <mail@baku89.com>
2020-05-05 13:42:22 +0900

    Try extracting mouse dragging feature as external function

--- src/components/input/InputVec2.vue
+++ src/components/input/InputVec2.vue
@@ -1,27 +1,87 @@
 <template>
 	<div class="InputVec2">
 		[
-		<InputNumber
-			class="InputVec2__el"
-			:value="value[0]"
-			@input="onInput(0, $event)"
-		/>
-		<InputNumber
-			class="InputVec2__el"
-			:value="value[1]"
-			@input="onInput(1, $event)"
-		/>]
-		<!-- <div class="InputVec2__drag" /> -->
+		<InputNumber class="InputVec2__el" :value="value[0]" @input="onInput(0, $event)" />
+		<InputNumber class="InputVec2__el" :value="value[1]" @input="onInput(1, $event)" />]
+		<button class="InputVec2__drag" :class="{dragging: drag.isDragging}" ref="dragEl" />
 	</div>
 </template>
 
 <script lang="ts">
-import {defineComponent, computed} from '@vue/composition-api'
-import {createMalVector} from '@/mal/types'
+import {
+	defineComponent,
+	computed,
+	onBeforeUnmount,
+	onMounted,
+	ref,
+	Ref,
+	watch,
+	reactive
+} from '@vue/composition-api'
+import {createMalVector, markMalVector} from '@/mal/types'
 import InputNumber from './InputNumber.vue'
 
 interface Props {
 	value: [number, number]
+}
+
+function markDraggable(el: Ref<null | HTMLElement>) {
+	const drag = reactive({
+		x: 0,
+		y: 0,
+		deltaX: 0,
+		deltaY: 0,
+		isDragging: false,
+		startX: 0,
+		startY: 0
+	})
+
+	let prevX: number, prevY: number
+
+	function onMousedrag(e: MouseEvent) {
+		const {clientX, clientY} = e
+
+		drag.x = clientX - drag.startX
+		drag.y = clientY - drag.startY
+		drag.deltaX = clientX - prevX
+		drag.deltaY = clientY - prevY
+		prevX = clientX
+		prevY = clientY
+	}
+
+	function onMouseup() {
+		drag.isDragging = false
+		drag.x = 0
+		drag.y = 0
+		drag.deltaX = 0
+		drag.deltaY = 0
+		drag.startX = 0
+		drag.startY = 0
+		window.removeEventListener('mousemove', onMousedrag)
+		window.removeEventListener('mouseup', onMouseup)
+	}
+
+	function onMousedown(e: MouseEvent) {
+		const {clientX, clientY} = e
+
+		drag.isDragging = true
+		drag.startX = clientX
+		drag.startY = clientY
+		prevX = clientX
+		prevY = clientY
+
+		window.addEventListener('mousemove', onMousedrag)
+		window.addEventListener('mouseup', onMouseup)
+	}
+
+	onBeforeUnmount(onMouseup)
+
+	watch(el, el => {
+		if (!(el instanceof HTMLElement)) return
+		el.addEventListener('mousedown', onMousedown)
+	})
+
+	return {drag}
 }
 
 export default defineComponent({
@@ -34,6 +94,8 @@ export default defineComponent({
 		}
 	},
 	setup(props: Props, context) {
+		const dragEl: Ref<null | HTMLElement> = ref(null)
+
 		const onInput = (i: number, v: number) => {
 			const value = createMalVector([...props.value])
 			value[i] = v
@@ -41,8 +103,22 @@ export default defineComponent({
 			context.emit('input', value)
 		}
 
+		const {drag} = markDraggable(dragEl)
+
+		watch(
+			() => [drag.isDragging, drag.deltaX, drag.deltaY],
+			([isDragging, x, y]) => {
+				const newValue = markMalVector([...props.value]) as number[]
+				newValue[0] += x as number
+				newValue[1] += y as number
+				context.emit('input', newValue)
+			}
+		)
+
 		return {
-			onInput
+			dragEl,
+			onInput,
+			drag
 		}
 	}
 })
@@ -65,9 +141,15 @@ export default defineComponent({
 	&__drag
 		position relative
 		margin-left 0.5rem
-		width 1.3rem
-		height 1.3rem
+		width 1.1rem
+		height @width
 		border 1px solid var(--comment)
+
+		&:hover, &.dragging
+			background var(--comment)
+
+			&:before, &:after
+				background var(--background)
 
 		&:before, &:after
 			position absolute
@@ -76,14 +158,14 @@ export default defineComponent({
 			content ''
 
 		&:before
-			top 10%
-			left 50%
+			top 2px
+			left calc(50% - 0.5px)
 			width 1px
-			height 80%
+			height calc(100% - 4px)
 
 		&:after
-			top 50%
-			left 10%
-			width 80%
+			top calc(50% - 0.5px)
+			left 2px
+			width calc(100% - 4px)
 			height 1px
 </style>
