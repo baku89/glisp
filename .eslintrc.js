0c62ec03296707a4f0ee2fa1ae0bc018363f2f18
Baku Hashimoto <mail@baku89.com>
2020-05-18 02:31:33 +0900

    Disable no-use-before-define rule of eslint properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-use-before-define': 'off',
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-use-before-define': 'off'
 	}
 }
