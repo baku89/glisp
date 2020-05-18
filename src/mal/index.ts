aa5de58d7910b71e008ae1c5cd33128eaf0503b6
Baku Hashimoto <mail@baku89.com>
2020-05-19 00:47:54 +0900

    Only expose readStr and printExp in mal/index.ts

--- src/mal/index.ts
+++ src/mal/index.ts
@@ -1,8 +1,4 @@
 import readStr from './reader'
 import printExp from './printer'
-import evalExp from './eval'
-import Env from './env'
 
-const readEvalStr = (str: string, env: Env) => evalExp(readStr(str), env)
-
-export {readStr, evalExp, printExp, readEvalStr}
+export {readStr, printExp}
