4ab26ed1038bef70bac11f8cd4e804c61c4c82d9
Baku Hashimoto <mail@baku89.com>
2020-05-21 22:09:03 +0900

    Add repl bundle config

---
+++ webpack.repl.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+	entry: './src/repl.ts',
+	resolve: {
+		alias: {
+			'@': '/Users/baku/Sites/glisp/src'
+		},
+		extensions: ['.tsx', '.ts', '.mjs', '.js', '.jsx', '.vue', '.json', '.wasm']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	target: 'node',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'repl')
+	}
+}
