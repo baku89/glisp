75b7ae3e1b895f5ec589524aac534af7e23bf5d0
Baku Hashimoto <mail@baku89.com>
2020-05-22 01:15:40 +0900

    Implement Node.js REPL loop

---
+++ src/repl.ts
@@ -0,0 +1,21 @@
+import readlineSync from 'readline-sync'
+import Scope from './mal/scope'
+
+const replScope = new Scope()
+
+replScope.REP(`(println (str "Mal [" *host-language* "]"))`)
+
+while (true) {
+	const line = readlineSync.question('glisp> ')
+	if (line == null) {
+		break
+	}
+	if (line === '') {
+		continue
+	}
+	try {
+		replScope.REP(line)
+	} catch (e) {
+		console.error('Error:', e)
+	}
+}
