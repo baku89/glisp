e11b6633f626f1111c7056d31a0b30c3e6eac064
Baku Hashimoto <mail@baku89.com>
2020-06-18 17:13:57 +0900

    Add Delaunay function

--- src/mal-lib/math.ts
+++ src/mal-lib/math.ts
@@ -1,11 +1,24 @@
 import {MalVal, symbolFor as S, markMalVector as V} from '@/mal/types'
 import hull from 'hull.js'
+import Delaunator from 'delaunator'
+import {partition} from '@/utils'
 
 const Exports = [
 	[
 		'convex-hull',
 		(pts: [number, number][], concavity: number | null = null) => {
 			return V(hull(pts, concavity === null ? Infinity : concavity))
+		}
+	],
+	[
+		'delaunay',
+		(pts: [number, number][]) => {
+			const delaunay = Delaunator.from(pts)
+			return V(
+				partition(3, delaunay.triangles).map(([a, b, c]) => {
+					return V([V([...pts[a]]), V([...pts[b]]), V([...pts[c]])])
+				})
+			)
 		}
 	]
 ] as [string, MalVal][]
