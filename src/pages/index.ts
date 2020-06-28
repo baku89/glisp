eda1341627f58b1fda2e92939240c882e7f6da41
Baku Hashimoto <mail@baku89.com>
2020-06-28 10:29:43 +0900

    Install VModal

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueCompositionApi from '@vue/composition-api'
 import App from '@/components/PageIndex.vue'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
+Vue.use(VModal, {dynamic: true})
 
 new Vue({
 	render: h => h(App)
