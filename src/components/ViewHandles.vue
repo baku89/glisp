d7160f2930e30025f83d3ca990f53544babe5cff
Baku Hashimoto <mail@baku89.com>
2020-07-01 14:46:54 +0900

    Adjust viewport guide

--- src/components/ViewHandles.vue
+++ src/components/ViewHandles.vue
@@ -30,11 +30,7 @@
 			<path class="ViewHandles__viewport-axis stroke" d="M -5000 0 H 5000" />
 			<path class="ViewHandles__viewport-axis stroke" d="M 0 -5000 V 5000" />
 		</g>
-		<g
-			v-if="handleCallbacks"
-			class="ViewHandles__axis"
-			:transform="axisTransform"
-		>
+		<g v-if="handleCallbacks" class="ViewHandles__axis" :transform="axisTransform">
 			<path class="stroke axis-x" marker-end="url(#arrow-x)" d="M 0 0 H 200" />
 			<path class="stroke axis-y" marker-end="url(#arrow-y)" d="M 0 0 V 200" />
 		</g>
@@ -66,19 +62,9 @@
 				/>
 				<template v-if="type === 'translate'">
 					<path class="stroke display" d="M 12 0 H -12" />
-					<path
-						class="stroke display"
-						:transform="yTransform"
-						d="M 0 12 V -12"
-					/>
+					<path class="stroke display" :transform="yTransform" d="M 0 12 V -12" />
 				</template>
-				<circle
-					class="fill display"
-					:class="cls"
-					cx="0"
-					cy="0"
-					:r="rem * 0.5"
-				/>
+				<circle class="fill display" :class="cls" cx="0" cy="0" :r="rem * 0.5" />
 			</template>
 		</g>
 	</svg>
@@ -650,7 +636,7 @@ export default defineComponent({
 
 	&__viewport-axis
 		stroke var(--guide) !important
-		stroke-dasharray 2 2
+		stroke-dasharray 1 4
 
 	// Hover behavior
 	*[hoverrable]:hover, *[dragging]
@@ -670,6 +656,9 @@ export default defineComponent({
 	// Classes
 	.dashed
 		stroke-dasharray 3 2
+
+	.axis-x, .axis-y
+		opacity 0.5
 
 	.axis-x, .axis-x .display
 		stroke var(--red) !important
