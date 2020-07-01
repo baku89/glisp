0660acb0e799dbafc9a2a34b026b0b6a7e167448
Baku Hashimoto <mail@baku89.com>
2020-07-01 15:43:37 +0900

    Enclose the state initialization inside the callback function of hotkeys-js

--- src/components/use/use-keyboard-state.ts
+++ src/components/use/use-keyboard-state.ts
@@ -1,14 +1,20 @@
-import {toRefs} from '@vue/composition-api'
+import {toRefs, reactive} from '@vue/composition-api'
 import hotkeys from 'hotkeys-js'
 import keycode from 'keycode'
 
-const state = toRefs({
-	shift: false,
-	alt: false,
-	ctrl: false
-} as {[keycode: string]: boolean})
+let state: any
 
 hotkeys('*', {keyup: true, keydown: true}, (e: KeyboardEvent) => {
+	if (!state) {
+		state = toRefs(
+			reactive({
+				shift: false,
+				alt: false,
+				ctrl: false
+			} as {[keycode: string]: boolean})
+		)
+	}
+
 	const code = keycode(e)
 	if (code in state) {
 		state[code].value = e.type === 'keydown'
