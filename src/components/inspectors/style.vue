743feea9d2171fd4632a480f6cbb441622101a6b
Baku Hashimoto <mail@baku89.com>
2020-07-17 19:13:59 +0900

    Add support for sorting, deleting to the style inspector

--- src/components/inspectors/style.vue
+++ src/components/inspectors/style.vue
@@ -1,17 +1,40 @@
 <template>
 	<div class="Inspector-style">
-		<div class="Inspector-style__style" v-for="(style, i) in styles" :key="i">
-			<ParamControl
-				:exp="style"
-				@input="onInputStyle($event, i)"
-				@select="$emit('select', $event)"
-			/>
-		</div>
-		<div class="Inspector-style__add">
-			<button class="Inspector-style__button" @click="addStyle('fill')">
+		<Draggable
+			:value="styles"
+			@input="sortStyles"
+			handle=".Inspector-style__handle"
+		>
+			<TransitionGroup>
+				<div
+					class="Inspector-style__style"
+					v-for="(style, i) in styles"
+					:key="i"
+				>
+					<ParamControl
+						:exp="style"
+						@input="updateStyleAt($event, i)"
+						@select="$emit('select', $event)"
+					/>
+					<i
+						class="Inspector-style__delete far fa-times-circle"
+						@click="deleteStyleAt(i)"
+					/>
+					<i class="Inspector-style__handle fa fa-align-justify handle"></i>
+				</div>
+			</TransitionGroup>
+		</Draggable>
+		<div class="Inspector-style__append">
+			<button
+				class="Inspector-style__append-button"
+				@click="appendStyle('fill')"
+			>
 				+ Add Fill
 			</button>
-			<button class="Inspector-style__button" @click="addStyle('stroke')">
+			<button
+				class="Inspector-style__append-button"
+				@click="appendStyle('stroke')"
+			>
 				+ Add Stroke
 			</button>
 		</div>
@@ -20,6 +43,7 @@
 
 <script lang="ts">
 import {defineComponent, SetupContext, computed} from '@vue/composition-api'
+import Draggable from 'vuedraggable'
 import {
 	MalVal,
 	isList,
@@ -31,6 +55,8 @@ import {
 } from '@/mal/types'
 import {NonReactive, nonReactive} from '@/utils'
 import ParamControl from '@/components/ParamControl.vue'
+import {printExp} from '../../mal'
+
 interface Props {
 	exp: NonReactive<MalVal[]>
 }
@@ -38,6 +64,7 @@ interface Props {
 export default defineComponent({
 	name: 'Inspector-style',
 	components: {
+		Draggable,
 		ParamControl
 	},
 	props: {
@@ -52,7 +79,7 @@ export default defineComponent({
 			return (isVector(styles) ? styles : [styles]).map(s => nonReactive(s))
 		})
 
-		function onInputStyle(style: NonReactive<MalSeq>, i: number) {
+		function updateStyleAt(style: NonReactive<MalSeq>, i: number) {
 			const newExp = cloneExp(props.exp.value)
 			const newStyles = styles.value.map(s => s.value)
 			newStyles[i] = style.value
@@ -61,7 +88,15 @@ export default defineComponent({
 			context.emit('input', nonReactive(newExp))
 		}
 
-		function addStyle(type: 'fill' | 'stroke') {
+		function sortStyles(styles: NonReactive<MalSeq>[]) {
+			const newExp = cloneExp(props.exp.value)
+			const newStyles = styles.map(s => s.value)
+			newExp[1] = newStyles.length == 1 ? newStyles[0] : newStyles
+
+			context.emit('input', nonReactive(newExp))
+		}
+
+		function appendStyle(type: 'fill' | 'stroke') {
 			const style =
 				type === 'fill' ? L(S('fill'), '#000000') : L(S('stroke'), '#000000', 1)
 
@@ -73,7 +108,16 @@ export default defineComponent({
 			context.emit('input', nonReactive(newExp))
 		}
 
-		return {styles, onInputStyle, addStyle}
+		function deleteStyleAt(i: number) {
+			const newExp = cloneExp(props.exp.value)
+			const newStyles = styles.value.map(s => s.value)
+			newStyles.splice(i, 1)
+			newExp[1] = newStyles.length == 1 ? newStyles[0] : newStyles
+
+			context.emit('input', nonReactive(newExp))
+		}
+
+		return {styles, updateStyleAt, sortStyles, appendStyle, deleteStyleAt}
 	}
 })
 </script>
@@ -83,14 +127,41 @@ export default defineComponent({
 
 .Inspector-style
 	&__style
+		display flex
+		align-items center
 		padding-bottom .5rem
 		margin-bottom .5rem
 		border-bottom 1px dotted var(--border)
 
-	&__add
+		& > *
+			display block
+
+	&__delete, &__handle
+		opacity 0
+		margin-right 0.5rem
+		transition opacity .05s ease
+
+		~/__style:hover &
+			opacity 1
+
+	&__delete
+		color var(--comment)
+		line-height $param-height
+		cursor pointer
+
+		&:hover
+			color var(--warning)
+
+	&__handle
+		color var(--comment)
+		height 100%
+		cursor all-scroll
+
+	&__append
 		display flex
 		justify-content center
-	&__button
+
+	&__append-button
 		cursor pointer
 		line-height $param-height
 		margin-top 0.3em
