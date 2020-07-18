34bf363347c031dc8b7c50977dea23aca0a71ec9
Baku Hashimoto <mail@baku89.com>
2020-07-18 15:41:51 +0900

    Add a new component MalInputParam

---
+++ src/components/mal-input/MalInputParam.vue
@@ -0,0 +1,93 @@
+<template>
+	<div class="MalInputParam">
+		<div class="MalInputParam__param" v-for="(scheme, i) in schemes" :key="i">
+			<div class="MalInputParam__label" v-if="scheme.label">
+				{{ scheme.label }}
+			</div>
+			<component
+				class="MalInputParam__input"
+				:is="scheme.type"
+				:value="params[i]"
+				:compact="true"
+				@input="onInputParam($event, i)"
+			/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import {defineComponent, computed, SetupContext} from '@vue/composition-api'
+import MalInputNumber from '@/components/mal-input/MalInputNumber.vue'
+import MalInputColor from '@/components/mal-input/MalInputColor.vue'
+import {
+	MalSeq,
+	getEvaluated,
+	MalType,
+	getMeta,
+	MalVal,
+	cloneExp
+} from '@/mal/types'
+import {getMapValue} from '@/mal/utils'
+import {convertMalNodeToJSObject} from '@/mal/reader'
+import {NonReactive, nonReactive} from '@/utils'
+
+interface Props {
+	value: NonReactive<MalSeq>
+}
+
+export default defineComponent({
+	name: 'MalInputParam',
+	components: {
+		number: MalInputNumber,
+		color: MalInputColor
+	},
+	props: {
+		value: {
+			required: true
+		}
+	},
+	setup(props: Props, context: SetupContext) {
+		const params = computed(() => {
+			return props.value.value.slice(1)
+		})
+
+		const fn = computed(() => getEvaluated(props.value.value[0]))
+
+		const schemes = computed(
+			() =>
+				convertMalNodeToJSObject(
+					getMapValue(getMeta(fn.value), 'compact-params', MalType.Vector)
+				) || null
+		)
+
+		function onInputParam(value: MalVal, i: number) {
+			const newExp = cloneExp(props.value.value)
+			newExp[i + 1] = value
+
+			context.emit('input', nonReactive(newExp))
+		}
+
+		return {params, schemes, onInputParam}
+	}
+})
+</script>
+
+<style lang="stylus">
+@import '../style/common.styl'
+
+.MalInputParam
+	display flex
+	align-items center
+	line-height $input-height
+
+	&__param
+		display flex
+		align-items center
+
+	&__label
+		margin-right .4rem
+
+
+	&__input
+		margin-right .6rem
+</style>
