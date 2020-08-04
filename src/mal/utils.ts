9c9e0202e197acc2bd76d9515897718d6b1c39a6
Baku Hashimoto <mail@baku89.com>
2020-08-04 13:43:30 +0900

    Adapt new system using :viewport-transform functions to compute the viewport transformation

--- src/mal/utils.ts
+++ src/mal/utils.ts
@@ -32,7 +32,7 @@ import {
 	getOuter,
 } from '@/mal/types'
 import ConsoleScope from '@/scopes/console'
-import {mat2d, vec2} from 'gl-matrix'
+import {mat2d} from 'gl-matrix'
 import {reconstructTree} from './reader'
 
 export function getStructType(exp: MalVal): StructTypes | undefined {
@@ -438,191 +438,44 @@ export function reverseEval(
 	return forceOverwrite ? exp : original
 }
 
-const K_TRANSFORM = K('transform')
-
-export function computeExpTransform(exp: MalVal) {
+export function computeExpTransform(exp: MalVal): mat2d {
 	if (!isNode(exp)) {
-		return mat2d.identity(mat2d.create())
+		return mat2d.create()
 	}
 
-	// Collect ancestors
-	let ancestors: MalNode[] = []
-	for (let outer: MalNode = exp; outer; outer = outer[M_OUTER]) {
-		ancestors.unshift(outer)
+	// Collect ancestors with index
+	const ancestors: [MalNode, number][] = []
+	for (let _exp: MalNode = exp; _exp[M_OUTER]; _exp = _exp[M_OUTER]) {
+		ancestors.unshift([_exp[M_OUTER], _exp[M_OUTER_INDEX]])
 	}
 
-	// NOTE: Might be too makeshift
-	if (isList(exp) && isSymbolFor(exp[0], 'path/transform')) {
-		ancestors.pop()
-	}
+	const xform = mat2d.create()
 
-	const attrMatrices: MalVal[] = []
-
-	// If the exp is nested inside transform arguments
-	for (let i = ancestors.length - 1; 0 < i; i--) {
-		const node = ancestors[i]
-		const outer = ancestors[i - 1]
-
-		if (!isList(outer)) {
+	for (const [node, index] of ancestors) {
+		if (!isList(node)) {
 			continue
 		}
 
-		const isAttrOfG =
-			isSymbolFor(outer[0], 'g') &&
-			outer[1] === node &&
-			isMap(node) &&
-			K_TRANSFORM in node
+		const meta = getMeta(getEvaluated(node[0]))
+		const viewportFn = getMapValue(meta, 'viewport-transform')
 
-		const isAttrOfTransform =
-			isSymbolFor(outer[0], 'transform') && outer[1] === node
-		const isAttrOfPathTransform =
-			isSymbolFor(outer[0], 'path/transform') && outer[1] === node
-		const isAttrOfArtboard =
-			isSymbolFor(outer[0], 'artboard') && outer[1] === node
-
-		if (
-			isAttrOfG ||
-			isAttrOfTransform ||
-			isAttrOfPathTransform ||
-			isAttrOfArtboard
-		) {
-			// Exclude attributes' part from ancestors
-			const attrAncestors = ancestors.slice(i)
-			ancestors = ancestors.slice(0, i - 1)
-
-			// Calculate transform compensation inside attribute
-			for (let j = attrAncestors.length - 1; 0 < j; j--) {
-				const node = attrAncestors[j]
-				const outer = attrAncestors[j - 1]
-
-				if (isList(outer)) {
-					if (isSymbolFor(outer[0], 'mat2d/*')) {
-						// Prepend matrices
-						const matrices = outer.slice(1, node[M_OUTER_INDEX])
-						attrMatrices.unshift(...matrices)
-					} else if (isSymbolFor(outer[0], 'pivot')) {
-						// Prepend matrices
-						const matrices = outer.slice(2, node[M_OUTER_INDEX])
-						attrMatrices.unshift(...matrices)
-
-						// Append pivot itself as translation matrix
-						const pivot =
-							isNode(outer[1]) && M_EVAL in outer[1]
-								? (outer[1][M_EVAL] as vec2)
-								: vec2.create()
-
-						const pivotMat = mat2d.fromTranslation(mat2d.create(), pivot)
-
-						attrMatrices.unshift(pivotMat as number[])
-					}
-				}
-			}
-
-			break
-		}
-	}
-
-	// Extract the matrices from ancestors
-	const matrices = ancestors.reduce((filtered, node) => {
-		if (isList(node)) {
-			if (
-				isSymbolFor(node[0], 'g') &&
-				isMap(node[1]) &&
-				K_TRANSFORM in node[1]
-			) {
-				const matrix = node[1][K_TRANSFORM]
-				filtered.push(matrix)
-			} else if (isSymbolFor(node[0], 'artboard')) {
-				const bounds = (node[1] as MalMap)[K('bounds')] as number[]
-				const matrix = [1, 0, 0, 1, ...bounds.slice(0, 2)]
-				filtered.push(matrix)
-			} else if (
-				isSymbolFor(node[0], 'transform') ||
-				isSymbolFor(node[0], 'path/transform')
-			) {
-				const matrix = node[1]
-				filtered.push(matrix)
-			}
+		if (!isFunc(viewportFn)) {
+			continue
 		}
 
-		return filtered
-	}, [] as MalVal[])
+		// Execute the viewport transform function
 
-	// Append attribute matrices
-	matrices.push(...attrMatrices)
+		const evaluatedParams = node.slice(1).map(x => getEvaluated(x))
+		const paramXforms = viewportFn(...evaluatedParams) as MalVal
 
-	// Multiplies all matrices in order
-	const ret = (matrices.map(xform => getEvaluated(xform)) as mat2d[]).reduce(
-		(xform, elXform) => mat2d.multiply(xform, xform, elXform),
-		mat2d.create()
-	)
-
-	return ret
-}
-
-const K_PARAMS = K('params')
-const K_REPLACE = K('replace')
-
-export function applyParamModifier(modifier: MalVal, originalParams: MalVal[]) {
-	if (!isVector(modifier) && !isMap(modifier)) {
-		return null
-	}
-
-	// Parse the modifier
-	let newParams: MalVal[]
-	let updatedIndices: number[] | undefined = undefined
-
-	if (isMap(modifier)) {
-		const params = modifier[K_PARAMS]
-		const replace = modifier[K_REPLACE]
-
-		if (isVector(params)) {
-			newParams = [...params]
-		} else if (isVector(replace)) {
-			newParams = [...originalParams]
-			const pairs = (typeof replace[0] === 'number'
-				? [(replace as any) as [number, MalVal]]
-				: ((replace as any) as [number, MalVal][])
-			).map(
-				([si, e]) =>
-					[si < 0 ? newParams.length + si : si, e] as [number, MalVal]
-			)
-			for (const [i, value] of pairs) {
-				newParams[i] = value
-			}
-			updatedIndices = pairs.map(([i]) => i)
-		} else {
-			return null
+		if (!isVector(paramXforms) || !paramXforms[index - 1]) {
+			continue
 		}
 
-		// if (isVector(changeId)) {
-		// 	const newId = newParams[1]
-		// 	data.draggingIndex = data.handles.findIndex(h => h.id === newId)
-		// }
-	} else {
-		newParams = modifier
+		mat2d.mul(xform, xform, paramXforms[index - 1] as mat2d)
 	}
 
-	if (!updatedIndices) {
-		updatedIndices = Array(newParams.length)
-			.fill(0)
-			.map((_, i) => i)
-	}
-
-	// Execute the backward evaluation
-	for (const i of updatedIndices) {
-		let newValue = newParams[i]
-		const unevaluated = originalParams[i]
-
-		// if (malEquals(newValue, this.params[i])) {
-		// 	newValue = unevaluated
-		// }
-
-		newValue = reverseEval(newValue, unevaluated)
-		newParams[i] = newValue
-	}
-
-	return newParams
+	return xform
 }
 
 export function getFn(exp: MalVal) {
