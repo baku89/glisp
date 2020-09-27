a4dba2de3425ff4dd41158b20111e50f9b755ede
Baku Hashimoto <mail@baku89.com>
2020-09-27 22:40:15 +0900

    Stop using reactive in useHandle

--- src/components/ViewHandles/use-handle.ts
+++ src/components/ViewHandles/use-handle.ts
@@ -19,18 +19,10 @@ import {
 	createList as L,
 	MalNode,
 } from '@/mal/types'
-import {
-	reactive,
-	computed,
-	Ref,
-	onBeforeMount,
-	SetupContext,
-	toRefs,
-} from 'vue'
+import {computed, Ref, onBeforeMount, SetupContext, ref} from 'vue'
 import {getSVGPathData, getSVGPathDataRecursive} from '@/path-utils'
 import {vec2, mat2d} from 'gl-matrix'
 import {NonReactive} from '@/utils'
-import {printExp} from '@/mal'
 import {reconstructTree} from '@/mal/reader'
 
 interface ClassList {
@@ -85,160 +77,157 @@ export default function useHandle(
 	viewEl: Ref<HTMLElement | null>,
 	context: SetupContext
 ) {
-	const data = reactive({
-		draggingIndex: null,
-		rawPrevPos: [0, 0],
-		fnInfo: computed(() => selectedExp.value.map(e => getFnInfo(e.value))),
-		handleCallbacks: computed(() =>
-			data.fnInfo.map(fi => {
-				if (!fi) {
-					return undefined
-				} else {
-					const ret = getMapValue(fi.meta, 'handles')
-					return isMap(ret) ? ret : undefined
+	const draggingIndex = ref<[number, number] | null>(null)
+	const rawPrevPos = ref([0, 0])
+	const fnInfo = computed(() => selectedExp.value.map(e => getFnInfo(e.value)))
+	const handleCallbacks = computed(() =>
+		fnInfo.value.map(fi => {
+			if (!fi) {
+				return undefined
+			} else {
+				const ret = getMapValue(fi.meta, 'handles')
+				return isMap(ret) ? ret : undefined
+			}
+		})
+	)
+	const params = computed(() =>
+		fnInfo.value.map((fi, i) => {
+			const e = selectedExp.value[i].value
+			if (isMap(e) || !fi) {
+				return []
+			}
+			return fi.structType
+				? [getEvaluated(e)]
+				: e.slice(1).map(e => getEvaluated(e))
+		})
+	)
+	const unevaluatedParams = computed(() =>
+		fnInfo.value.map((fi, i) => {
+			const e = selectedExp.value[i].value
+			if (isMap(e) || !fi) {
+				return []
+			}
+			return fi.structType ? [e] : e.slice(1)
+		})
+	)
+	const returnedValue = computed(() =>
+		selectedExp.value.map(e => getEvaluated(e.value))
+	)
+	const transform = computed(() =>
+		selectedExp.value.map((e, i) => {
+			const xform = computeExpTransform(e.value)
+			// pre-multiplies with viewTransform
+			mat2d.multiply(xform, viewTransform.value as mat2d, xform)
+
+			return xform
+		})
+	)
+	const transformInv = computed(() =>
+		transform.value.map(xform => mat2d.invert(mat2d.create(), xform))
+	)
+	const transformStyle = computed(() =>
+		transform.value.map(xform => `matrix(${xform.join(',')})`)
+	)
+	const selectedPath = computed(() =>
+		returnedValue.value.map(getSVGPathDataRecursive)
+	)
+	const handles = computed(() =>
+		handleCallbacks.value.map((cb, index) => {
+			if (!cb) return []
+
+			const drawHandle = cb[K_DRAW]
+
+			if (typeof drawHandle !== 'function') {
+				return []
+			}
+
+			const options = {
+				[K_PARAMS]: params.value[index],
+				[K_RETURN]: returnedValue.value[index],
+			}
+
+			let handles
+			try {
+				handles = drawHandle(options)
+			} catch (err) {
+				console.error('ViewHandles draw', err)
+				return []
+			}
+
+			if (!isVector(handles)) {
+				return []
+			}
+
+			return handles.map((h: any) => {
+				const type = h[K_TYPE] as string
+				const guide = !!h[K_GUIDE]
+				const classList = ((h[K_CLASS] as string) || '')
+					.split(' ')
+					.filter(c => !!c)
+				const cls = {} as ClassList
+				for (const name of classList) {
+					cls[name] = true
 				}
-			})
-		),
-		params: computed(() =>
-			data.fnInfo.map((fnInfo, i) => {
-				const e = selectedExp.value[i].value
-				if (isMap(e) || !fnInfo) {
-					return []
-				}
-				return fnInfo.structType
-					? [getEvaluated(e)]
-					: e.slice(1).map(e => getEvaluated(e))
-			})
-		),
-		unevaluatedParams: computed(() =>
-			data.fnInfo.map((fnInfo, i) => {
-				const e = selectedExp.value[i].value
-				if (isMap(e) || !fnInfo) {
-					return []
-				}
-				return fnInfo.structType ? [e] : e.slice(1)
-			})
-		),
-		returnedValue: computed(() =>
-			selectedExp.value.map(e => getEvaluated(e.value))
-		),
-		transform: computed(() =>
-			selectedExp.value.map((e, i) => {
-				const xform = computeExpTransform(e.value)
 
-				// pre-multiplies with viewTransform
-				mat2d.multiply(xform, viewTransform.value as mat2d, xform)
+				const xform = mat2d.clone(transform.value[index])
+				let yRotate = 0
 
-				return xform
-			})
-		),
-		transformInv: computed(() =>
-			data.transform.map(xform => mat2d.invert(mat2d.create(), xform))
-		),
-		transformStyle: computed(() =>
-			data.transform.map(xform => `matrix(${xform.join(',')})`)
-		),
-		selectedPath: computed(() =>
-			data.returnedValue.map(getSVGPathDataRecursive)
-		),
-		handles: computed(() =>
-			data.handleCallbacks.map((cb, index) => {
-				if (!cb) return []
-
-				const drawHandle = cb[K_DRAW]
-
-				if (typeof drawHandle !== 'function') {
-					return []
+				if (POINTABLE_HANDLE_TYPES.has(type)) {
+					const [x, y] = h[K_POS]
+					mat2d.translate(xform, xform, [x, y])
 				}
 
-				const options = {
-					[K_PARAMS]: data.params[index],
-					[K_RETURN]: data.returnedValue[index],
+				if (type === 'arrow') {
+					const angle = h[K_ANGLE] || 0
+					mat2d.rotate(xform, xform, angle)
+				} else if (type === 'dia') {
+					xform[0] = 1
+					xform[1] = 0
+					xform[2] = 0
+					xform[3] = 1
 				}
 
-				let handles
-				try {
-					handles = drawHandle(options)
-				} catch (err) {
-					console.error('ViewHandles draw', err)
-					return []
-				}
+				if (type !== 'path') {
+					// Normalize axis X
+					const axis = vec2.fromValues(xform[0], xform[1])
+					vec2.normalize(axis, axis)
+					xform[0] = axis[0]
+					xform[1] = axis[1]
 
-				if (!isVector(handles)) {
-					return []
-				}
+					// Force axisY to be perpendicular to axisX
+					const origAxisY = vec2.fromValues(xform[2], xform[3])
 
-				return handles.map((h: any) => {
-					const type = h[K_TYPE] as string
-					const guide = !!h[K_GUIDE]
-					const classList = ((h[K_CLASS] as string) || '')
-						.split(' ')
-						.filter(c => !!c)
-					const cls = {} as ClassList
-					for (const name of classList) {
-						cls[name] = true
-					}
+					vec2.set(axis, xform[0], xform[1])
+					vec2.rotate(axis, axis, [0, 0], Math.PI / 2)
+					xform[2] = axis[0]
+					xform[3] = axis[1]
 
-					const xform = mat2d.clone(data.transform[index])
-					let yRotate = 0
-
-					if (POINTABLE_HANDLE_TYPES.has(type)) {
-						const [x, y] = h[K_POS]
-						mat2d.translate(xform, xform, [x, y])
-					}
-
-					if (type === 'arrow') {
-						const angle = h[K_ANGLE] || 0
-						mat2d.rotate(xform, xform, angle)
-					} else if (type === 'dia') {
-						xform[0] = 1
-						xform[1] = 0
-						xform[2] = 0
-						xform[3] = 1
-					}
-
-					if (type !== 'path') {
-						// Normalize axis X
-						const axis = vec2.fromValues(xform[0], xform[1])
-						vec2.normalize(axis, axis)
-						xform[0] = axis[0]
-						xform[1] = axis[1]
-
-						// Force axisY to be perpendicular to axisX
-						const origAxisY = vec2.fromValues(xform[2], xform[3])
-
-						vec2.set(axis, xform[0], xform[1])
-						vec2.rotate(axis, axis, [0, 0], Math.PI / 2)
-						xform[2] = axis[0]
-						xform[3] = axis[1]
-
-						// set Y rotation
-						if (type === 'translate') {
-							const perpAxisYAngle = Math.atan2(axis[1], axis[0])
-							vec2.rotate(axis, origAxisY, [0, 0], -perpAxisYAngle)
-							yRotate = Math.atan2(axis[1], axis[0])
-						}
-					}
-
-					const ret: Handle = {
-						type,
-						cls,
-						guide,
-						id: h[K_ID],
-						transform: `matrix(${xform.join(',')})`,
-					}
-
+					// set Y rotation
 					if (type === 'translate') {
-						ret.yTransform = `rotate(${(yRotate * 180) / Math.PI})`
-					} else if (type === 'path') {
-						ret.path = getSVGPathData(h[K_PATH])
+						const perpAxisYAngle = Math.atan2(axis[1], axis[0])
+						vec2.rotate(axis, origAxisY, [0, 0], -perpAxisYAngle)
+						yRotate = Math.atan2(axis[1], axis[0])
 					}
+				}
 
-					return ret
-				})
+				const ret: Handle = {
+					type,
+					cls,
+					guide,
+					id: h[K_ID],
+					transform: `matrix(${xform.join(',')})`,
+				}
+
+				if (type === 'translate') {
+					ret.yTransform = `rotate(${(yRotate * 180) / Math.PI})`
+				} else if (type === 'path') {
+					ret.path = getSVGPathData(h[K_PATH])
+				}
+
+				return ret
 			})
-		),
-	}) as HandleData
+		})
+	)
 
 	function onMousedown(
 		[selectedIndex, handleIndex]: [number, number],
@@ -246,27 +235,31 @@ export default function useHandle(
 	) {
 		if (!viewEl.value) return
 
-		data.draggingIndex = [selectedIndex, handleIndex]
+		draggingIndex.value = [selectedIndex, handleIndex]
 		const {left, top} = viewEl.value.getBoundingClientRect()
-		data.rawPrevPos = [e.clientX - left, e.clientY - top]
+		rawPrevPos.value = [e.clientX - left, e.clientY - top]
 
 		window.addEventListener('mousemove', onMousedrag)
 		window.addEventListener('mouseup', onMouseup)
 	}
 
 	function onMousedrag(e: MouseEvent) {
-		if (!data.handleCallbacks || data.draggingIndex === null || !viewEl.value) {
+		if (
+			!handleCallbacks.value ||
+			draggingIndex.value === null ||
+			!viewEl.value
+		) {
 			return
 		}
 
-		const [selectedIndex, handleIndex] = data.draggingIndex
-		const handleCallbacks = data.handleCallbacks[selectedIndex]
+		const [selectedIndex, handleIndex] = draggingIndex.value
+		const cbs = handleCallbacks.value[selectedIndex]
 
-		if (!handleCallbacks) {
+		if (!cbs) {
 			return
 		}
 
-		const dragHandle = handleCallbacks[K_DRAG]
+		const dragHandle = cbs[K_DRAG]
 
 		if (typeof dragHandle !== 'function') {
 			return
@@ -275,32 +268,32 @@ export default function useHandle(
 		const viewRect = viewEl.value.getBoundingClientRect()
 		const rawPos = [e.clientX - viewRect.left, e.clientY - viewRect.top]
 
-		const transformInv = data.transformInv[selectedIndex]
-		const fnInfo = data.fnInfo[selectedIndex]
+		const xformInv = transformInv.value[selectedIndex]
+		const fi = fnInfo.value[selectedIndex]
 		const exp = selectedExp.value[selectedIndex].value
-		const params = data.params[selectedIndex]
-		const unevaluatedParams = data.unevaluatedParams[selectedIndex]
+		const _params = params.value[selectedIndex]
+		const _unevaluatedParams = unevaluatedParams.value[selectedIndex]
 
-		if (!fnInfo || isMap(exp)) {
+		if (!fi || isMap(exp)) {
 			return
 		}
 
 		const pos = [0, 0]
-		vec2.transformMat2d(pos as vec2, rawPos as vec2, transformInv)
+		vec2.transformMat2d(pos as vec2, rawPos as vec2, xformInv)
 
 		const prevPos = [0, 0]
-		vec2.transformMat2d(prevPos as vec2, data.rawPrevPos as vec2, transformInv)
+		vec2.transformMat2d(prevPos as vec2, rawPrevPos.value as vec2, xformInv)
 
-		const handle = data.handles[selectedIndex][handleIndex]
+		const handle = handles.value[selectedIndex][handleIndex]
 
 		const eventInfo = {
 			[K_ID]: handle.id === undefined ? null : handle.id,
 			[K_POS]: pos,
 			[K_PREV_POS]: prevPos,
-			[K_PARAMS]: params,
+			[K_PARAMS]: _params,
 		} as MalMap
 
-		data.rawPrevPos = rawPos
+		rawPrevPos.value = rawPos
 
 		let result: MalVal
 		try {
@@ -326,7 +319,7 @@ export default function useHandle(
 			if (isVector(retParams)) {
 				newParams = retParams
 			} else if (isVector(replace)) {
-				newParams = [...unevaluatedParams]
+				newParams = [..._unevaluatedParams]
 				const pairs = (typeof replace[0] === 'number'
 					? [(replace as any) as [number, MalVal]]
 					: ((replace as any) as [number, MalVal][])
@@ -344,9 +337,9 @@ export default function useHandle(
 
 			if (isVector(changeId)) {
 				const newId = newParams[1]
-				data.draggingIndex = [
+				draggingIndex.value = [
 					selectedIndex,
-					data.handles[selectedIndex].findIndex(h => malEquals(h.id, newId)),
+					handles.value[selectedIndex].findIndex(h => malEquals(h.id, newId)),
 				]
 			}
 		} else {
@@ -362,18 +355,14 @@ export default function useHandle(
 		// Execute the backward evaluation
 		for (const i of updatedIndices) {
 			let newValue = newParams[i]
-			const unevaluated = unevaluatedParams[i]
-
-			// if (malEquals(newValue, this.params[i])) {
-			// 	newValue = unevaluated
-			// }
+			const unevaluated = _unevaluatedParams[i]
 
 			newValue = reverseEval(newValue, unevaluated)
 			newParams[i] = newValue
 		}
 
 		// Construct the new expression and send it to parent
-		const newExp: MalSeq = fnInfo.structType
+		const newExp: MalSeq = fi.structType
 			? (newParams[0] as MalSeq)
 			: (L(exp[0], ...newParams) as MalSeq)
 
@@ -386,7 +375,7 @@ export default function useHandle(
 	}
 
 	function onMouseup() {
-		data.draggingIndex = null
+		draggingIndex.value = null
 		unregisterMouseEvents()
 		context.emit('tag-history', 'undo')
 	}
@@ -401,7 +390,8 @@ export default function useHandle(
 	})
 
 	return {
-		...toRefs(data as any),
+		draggingIndex,
+		handles,
 		onMousedown,
 	}
 }
