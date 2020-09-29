75a9ec8a9e5a3f12e39f6381c6edcc6e10c78f06
Baku Hashimoto <mail@baku89.com>
2020-09-29 15:29:05 +0900

    Use ResizeObserver in useResizeSensor

--- src/components/use/use-resize-sensor.ts
+++ src/components/use/use-resize-sensor.ts
@@ -1,29 +1,33 @@
+/// <reference types="@types/resize-observer-browser" />
 import {Ref, onMounted, onBeforeMount, unref} from 'vue'
-import ResizeSensor from 'resize-sensor'
 
 export default function useResizeSensor(
 	element: Ref<HTMLElement | null> | HTMLElement,
 	onResized: (el: HTMLElement) => any,
 	immediate = false
 ) {
-	let sensor: any
+	let el: HTMLElement
+	let observer: ResizeObserver
 
-	function setup(el: HTMLElement) {
-		sensor = new ResizeSensor(el, () => onResized(el))
+	onMounted(() => {
+		const _el = unref(element)
+		if (!_el) return
+
+		el = _el
+
+		observer = new ResizeObserver(() => {
+			onResized(el)
+		})
+		observer.observe(el)
+
 		if (immediate) {
 			onResized(el)
 		}
-	}
-
-	onMounted(() => {
-		const el = unref(element)
-		if (!el) return
-		setup(el)
 	})
 
 	onBeforeMount(() => {
-		if (sensor) {
-			sensor.detach()
+		if (observer) {
+			observer.unobserve(el)
 		}
 	})
 }
