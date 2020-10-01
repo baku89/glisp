789f2023927165ad725833a81a5d24418c6a6080
Baku Hashimoto <mail@baku89.com>
2020-10-01 12:25:19 +0900

    Comment out whole use-dialog-command.ts

--- src/components/PageIndex/use/use-dialog-command.ts
+++ src/components/PageIndex/use/use-dialog-command.ts
@@ -1,61 +1,61 @@
-import {SetupContext} from 'vue'
-import ConsoleScope from '@/scopes/console'
-import {
-	MalVal,
-	MalError,
-	MalSymbol,
-	MalMap,
-	MalFunc,
-	MalType,
-	// MalList,
-} from '@/mal/types'
+// import {SetupContext} from 'vue'
+// import ConsoleScope from '@/scopes/console'
+// import {
+// 	MalVal,
+// 	MalError,
+// 	MalSymbol,
+// 	MalMap,
+// 	MalFunc,
+// 	MalType,, MalVector
+// 	// MalList,
+// } from '@/mal/types'
 
-import {printExp} from '@/mal'
-// import DialogCommand from '@/components/dialogs/DialogCommand.vue'
-import {getMapValue} from '@/mal/utils'
-import {printer} from '@/mal/printer'
-//
-export default function useDialogCommand(context: SetupContext) {
-	ConsoleScope.defn('show-command-dialog', (f: MalVal) => {
-		if (f === undefined || !MalSymbol.is(f)) {
-			throw new MalError(`${printExp(f)} is not a symbol`)
-		}
+// import {printExp} from '@/mal'
+// // import DialogCommand from '@/components/dialogs/DialogCommand.vue'
+// import {getMapValue} from '@/mal/utils'
+// import {printer} from '@/mal/printer'
+// //
+// export default function useDialogCommand(context: SetupContext) {
+// 	ConsoleScope.defn('show-command-dialog', (f: MalVal) => {
+// 		if (f === undefined || !MalSymbol.is(f)) {
+// 			throw new MalError(`${printExp(f)} is not a symbol`)
+// 		}
 
-		// Retrieve default parameters
-		const fn = ConsoleScope.var(f.value)
-		const meta = getMeta(fn)
-		const paramsDesc = getMapValue(meta, 'params', MalType.Vector) as
-			| MalMap[]
-			| null
-		let initialParams = getMapValue(meta, 'initial-params') as
-			| MalFunc
-			| MalVal[]
-			| null
+// 		// Retrieve default parameters
+// 		const fn = ConsoleScope.var(f.value)
+// 		const meta = fn.meta
+// 		const paramsDesc = getMapValue(meta, 'params', MalType.Vector) as
+// 			| MalMap[]
+// 			| null
+// 		let initialParams = getMapValue(meta, 'initial-params') as
+// 			| MalFunc
+// 			| MalVal[]
+// 			| null
 
-		if (!paramsDesc || !initialParams) {
-			printer.error('NO initial parmas')
-			return null
-		}
+// 		if (!paramsDesc || !initialParams) {
+// 			printer.error('NO initial parmas')
+// 			return null
+// 		}
 
-		if (isFunc(initialParams)) {
-			initialParams = initialParams() as MalVal[]
-		}
+// 		if (MalFunc.is(initialParams)) {
+// 			initialParams = initialParams.value() as MalVector
+// 		}
 
-		context
+// 		context
 
-		// Create the expression with default parameters
-		// const exp: MalVal = nonReactive(L(f, ...initialParams))
+// 		// Create the expression with default parameters
+// 		// const exp: MalVal = nonReactive(L(f, ...initialParams))
 
-		// Show Modal
-		// $modal.show(
-		// 	DialogCommand,
-		// 	{
-		// 		exp,
-		// 		fn,
-		// 	},
-		// 	{}
-		// )
+// 		// Show Modal
+// 		// $modal.show(
+// 		// 	DialogCommand,
+// 		// 	{
+// 		// 		exp,
+// 		// 		fn,
+// 		// 	},
+// 		// 	{}
+// 		// )
 
-		return null
-	})
-}
+// 		return null
+// 	})
+// }
