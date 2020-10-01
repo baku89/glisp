29fa0e26090585743e2ef9153f28ae7ea5a90c36
Baku Hashimoto <mail@baku89.com>
2020-10-01 16:18:40 +0900

    Fix console.ts

--- src/scopes/console.ts
+++ src/scopes/console.ts
@@ -51,22 +51,22 @@ ConsoleScope.defn('copy-to-clipboard', (str: MalVal) => {
 	return copyToClipboard(str.value as string)
 })
 
-ConsoleScope.def(
-	'generate-sketch-url',
-	MalFunc.create(
-		(url: MalVal) => generateSketchURL(url.value as string),
-		jsToMal({
-			doc: 'Generates Code URL',
-			params: [
-				{
-					label: 'Source',
-					type: 'string',
-				},
-			],
-			'initial-params': [''],
-		}) as MalMap
-	)
-)
+// ConsoleScope.def(
+// 	'generate-sketch-url',
+// 	MalFunc.create(
+// 		(url: MalVal) => generateSketchURL(url.value as string),
+// 		jsToMal({
+// 			doc: 'Generates Code URL',
+// 			params: [
+// 				{
+// 					label: 'Source',
+// 					type: 'string',
+// 				},
+// 			],
+// 			'initial-params': [''],
+// 		}) as MalMap
+// 	)
+// )
 
 ConsoleScope.defn('open-link', (url: MalVal) => {
 	window.open(url.value as string, '_blank')
@@ -79,7 +79,7 @@ ConsoleScope.defn('clear-console', () => {
 })
 
 ConsoleScope.defn('download-sketch', (...args: MalVal[]) => {
-	const filename = generateFilename(args[0] as string)
+	const filename = generateFilename(args[0].value as string)
 
 	const sketch = ConsoleScope.var('*sketch*').value as string
 
@@ -103,338 +103,338 @@ ConsoleScope.defn('copy-as-svg', () => {
 const renderViewScope = createViewScope()
 let renderWindow: Window | null
 
-ConsoleScope.defn(
-	'export-image',
-	setMeta(
-		(...xs: MalVal[]) => {
-			const exec = async () => {
-				const sketch = ConsoleScope.var('*sketch*').value as string
-				const code = `(sketch ${sketch}\nnil)`
+// ConsoleScope.defn(
+// 	'export-image',
+// 	setMeta(
+// 		(...xs: MalVal[]) => {
+// 			const exec = async () => {
+// 				const sketch = ConsoleScope.var('*sketch*').value as string
+// 				const code = `(sketch ${sketch}\nnil)`
 
-				renderViewScope.setup({guideColor: null})
-				let viewExp = renderViewScope.readEval(code)
+// 				renderViewScope.setup({guideColor: null})
+// 				let viewExp = renderViewScope.readEval(code)
 
-				if (viewExp === undefined) {
-					throw new MalError('Invalid sketch')
-				}
+// 				if (viewExp === undefined) {
+// 					throw new MalError('Invalid sketch')
+// 				}
 
-				const options = MalMap.fromMalSeq(...xs).value
+// 				const options = MalMap.fromMalSeq(...xs).value
 
-				if (options.selector) {
-					viewExp = ConsoleScope.eval(
-						MalList.create(
-							MalSymbol.create('filter-elements'),
-							options.selector,
-							viewExp
-						)
-					)
-					if (!viewExp) {
-						throw new MalError(
-							`Element ${printExp(options.selector, true)} does not exist`
-						)
-					}
-				}
+// 				if (options.selector) {
+// 					viewExp = ConsoleScope.eval(
+// 						MalList.create(
+// 							MalSymbol.create('filter-elements'),
+// 							options.selector,
+// 							viewExp
+// 						)
+// 					)
+// 					if (!viewExp) {
+// 						throw new MalError(
+// 							`Element ${printExp(options.selector, true)} does not exist`
+// 						)
+// 					}
+// 				}
 
-				const bounds = ConsoleScope.eval(
-					MalList.create(MalSymbol.create('get-element-bounds'), viewExp)
-				)?.toJS() as number[]
-				if (!bounds) {
-					throw new MalError('Cannot retrieve bounds')
-				}
+// 				const bounds = ConsoleScope.eval(
+// 					MalList.create(MalSymbol.create('get-element-bounds'), viewExp)
+// 				)?.toJS() as number[]
+// 				if (!bounds) {
+// 					throw new MalError('Cannot retrieve bounds')
+// 				}
 
-				const image = await getRendereredImage(viewExp, {
-					format: options.format.value as string,
-					scaling: options.scaling.value as number,
-					bounds,
-				})
+// 				const image = await getRendereredImage(viewExp, {
+// 					format: options.format.value as string,
+// 					scaling: options.scaling.value as number,
+// 					bounds,
+// 				})
 
-				if (renderWindow) {
-					renderWindow.close()
-				}
+// 				if (renderWindow) {
+// 					renderWindow.close()
+// 				}
 
-				renderWindow = window.open('about:blank', 'Image for canvas')
-				renderWindow?.document.write(`<img src=${image} />`)
+// 				renderWindow = window.open('about:blank', 'Image for canvas')
+// 				renderWindow?.document.write(`<img src=${image} />`)
 
-				console.timeEnd('render')
-			}
+// 				console.timeEnd('render')
+// 			}
 
-			exec()
+// 			exec()
 
-			return MalNil.create()
-		},
-		jsToMal({
-			doc: 'Renders and exports a sketch',
-			params: [
-				{
-					type: 'map',
-					variadic: 'true',
-					items: [
-						{
-							key: MalKeyword.create('format'),
-							type: 'string',
-							ui: 'dropdown',
-							values: ['png', 'jpeg', 'webp'],
-						},
-						{
-							key: MalKeyword.create('scaling'),
-							type: 'number',
-							default: 1,
-							validator: (x: number) => Math.round(Math.max(1, x) * 2) / 2,
-						},
-						{key: MalKeyword.create('selector'), type: 'string', default: ''},
-					],
-				},
-			],
-			'initial-params': [
-				MalKeyword.create('format'),
-				'png',
-				MalKeyword.create('scaling'),
-				1,
-				MalKeyword.create('selector'),
-				'',
-			],
-		})
-	)
-)
+// 			return MalNil.create()
+// 		},
+// 		jsToMal({
+// 			doc: 'Renders and exports a sketch',
+// 			params: [
+// 				{
+// 					type: 'map',
+// 					variadic: 'true',
+// 					items: [
+// 						{
+// 							key: MalKeyword.create('format'),
+// 							type: 'string',
+// 							ui: 'dropdown',
+// 							values: ['png', 'jpeg', 'webp'],
+// 						},
+// 						{
+// 							key: MalKeyword.create('scaling'),
+// 							type: 'number',
+// 							default: 1,
+// 							validator: (x: number) => Math.round(Math.max(1, x) * 2) / 2,
+// 						},
+// 						{key: MalKeyword.create('selector'), type: 'string', default: ''},
+// 					],
+// 				},
+// 			],
+// 			'initial-params': [
+// 				MalKeyword.create('format'),
+// 				'png',
+// 				MalKeyword.create('scaling'),
+// 				1,
+// 				MalKeyword.create('selector'),
+// 				'',
+// 			],
+// 		})
+// 	)
+// )
 
-ConsoleScope.defn(
-	'export-video',
-	setMeta(
-		(...xs: MalVal[]) => {
-			const options = {
-				format: 'gif',
-				scaling: 1,
-				symbol: 'time',
-				start: 0,
-				duration: 1,
-				fps: 24,
-				bounds: [0, 0, 200, 200],
-				...MalMap.fromMalSeq(...xs).toJS(),
-			} as {
-				format: 'gif'
-				scaling: number
-				symbol: string
-				start: number
-				duration: number
-				fps: number
-				bounds: number[]
-			}
+// ConsoleScope.defn(
+// 	'export-video',
+// 	setMeta(
+// 		(...xs: MalVal[]) => {
+// 			const options = {
+// 				format: 'gif',
+// 				scaling: 1,
+// 				symbol: 'time',
+// 				start: 0,
+// 				duration: 1,
+// 				fps: 24,
+// 				bounds: [0, 0, 200, 200],
+// 				...MalMap.fromMalSeq(...xs).toJS(),
+// 			} as {
+// 				format: 'gif'
+// 				scaling: number
+// 				symbol: string
+// 				start: number
+// 				duration: number
+// 				fps: number
+// 				bounds: number[]
+// 			}
 
-			const renderTime = async (time: number) => {
-				const sketch = ConsoleScope.var('*sketch*').value as string
-				const code = `(sketch-at-time "${options.symbol}" ${time} ${sketch}\nnil)`
+// 			const renderTime = async (time: number) => {
+// 				const sketch = ConsoleScope.var('*sketch*').value as string
+// 				const code = `(sketch-at-time "${options.symbol}" ${time} ${sketch}\nnil)`
 
-				renderViewScope.setup({guideColor: null})
-				const viewExp = renderViewScope.readEval(code)
-				if (viewExp === undefined) {
-					throw new MalError('Invalid sketch')
-				}
+// 				renderViewScope.setup({guideColor: null})
+// 				const viewExp = renderViewScope.readEval(code)
+// 				if (viewExp === undefined) {
+// 					throw new MalError('Invalid sketch')
+// 				}
 
-				const image = await getRendereredImage(viewExp, {
-					format: options.format,
-					scaling: options.scaling,
-					bounds: options.bounds,
-				})
+// 				const image = await getRendereredImage(viewExp, {
+// 					format: options.format,
+// 					scaling: options.scaling,
+// 					bounds: options.bounds,
+// 				})
 
-				return image
-			}
+// 				return image
+// 			}
 
-			const exec = async () => {
-				const gif = new GIF({workers: 2, quality: 10})
+// 			const exec = async () => {
+// 				const gif = new GIF({workers: 2, quality: 10})
 
-				const startTime = performance.now()
+// 				const startTime = performance.now()
 
-				const frameCount = Math.round(options.duration * options.fps)
-				const frameDuration = 1 / options.fps
-				const times = Array(frameCount)
-					.fill(0)
-					.map((_, i) => [i * frameDuration, i])
+// 				const frameCount = Math.round(options.duration * options.fps)
+// 				const frameDuration = 1 / options.fps
+// 				const times = Array(frameCount)
+// 					.fill(0)
+// 					.map((_, i) => [i * frameDuration, i])
 
-				for (const [time, i] of times) {
-					const data = (await renderTime(time)) as string
-					const img = new Image()
-					img.src = data
-					img.width = options.bounds[2] * options.scaling
-					img.height = options.bounds[3] * options.scaling
-					gif.addFrame(img, {delay: frameDuration * 1000})
+// 				for (const [time, i] of times) {
+// 					const data = (await renderTime(time)) as string
+// 					const img = new Image()
+// 					img.src = data
+// 					img.width = options.bounds[2] * options.scaling
+// 					img.height = options.bounds[3] * options.scaling
+// 					gif.addFrame(img, {delay: frameDuration * 1000})
 
-					printer.log(`Rendering... ${i + 1}/${frameCount} Frames`)
-				}
+// 					printer.log(`Rendering... ${i + 1}/${frameCount} Frames`)
+// 				}
 
-				gif.on('finished', (blob: Blob) => {
-					const deltaTime = (performance.now() - startTime) / 1000
-					printer.log(`Render finished. ${deltaTime.toFixed(1)}s`)
+// 				gif.on('finished', (blob: Blob) => {
+// 					const deltaTime = (performance.now() - startTime) / 1000
+// 					printer.log(`Render finished. ${deltaTime.toFixed(1)}s`)
 
-					window.open(URL.createObjectURL(blob))
-				})
+// 					window.open(URL.createObjectURL(blob))
+// 				})
 
-				gif.render()
-			}
+// 				gif.render()
+// 			}
 
-			exec()
+// 			exec()
 
-			return MalNil.create()
-		},
-		jsToMal({
-			doc: 'Exports a video',
-			params: [
-				{
-					type: 'map',
-					variadic: true,
-					items: [
-						{
-							key: MalKeyword.create('format'),
-							type: 'string',
-							ui: 'dropdown',
-							values: ['gif'],
-						},
-						{
-							key: MalKeyword.create('scaling'),
-							type: 'number',
-							default: 1,
-							validator: (x: number) => Math.round(Math.max(1, x) * 2) / 2,
-						},
-						{key: MalKeyword.create('symbol'), type: 'string'},
-						{key: MalKeyword.create('start'), type: 'number', default: 0},
-						{key: MalKeyword.create('duration'), type: 'number', default: 1},
-						{
-							key: MalKeyword.create('fps'),
-							label: 'FPS',
-							type: 'number',
-							default: 24,
-						},
-						{
-							key: MalKeyword.create('bounds'),
-							type: 'rect2d',
-							default: [0, 0, 100, 100],
-						},
-					],
-				},
-			],
-			'initial-params': [
-				MalKeyword.create('format'),
-				'gif',
-				MalKeyword.create('scaling'),
-				1,
-				MalKeyword.create('symbol'),
-				'time',
-				MalKeyword.create('start'),
-				0,
-				MalKeyword.create('duration'),
-				1,
-				MalKeyword.create('fps'),
-				24,
-				MalKeyword.create('bounds'),
-				[0, 0, 200, 200],
-			],
-		})
-	)
-)
+// 			return MalNil.create()
+// 		},
+// 		jsToMal({
+// 			doc: 'Exports a video',
+// 			params: [
+// 				{
+// 					type: 'map',
+// 					variadic: true,
+// 					items: [
+// 						{
+// 							key: MalKeyword.create('format'),
+// 							type: 'string',
+// 							ui: 'dropdown',
+// 							values: ['gif'],
+// 						},
+// 						{
+// 							key: MalKeyword.create('scaling'),
+// 							type: 'number',
+// 							default: 1,
+// 							validator: (x: number) => Math.round(Math.max(1, x) * 2) / 2,
+// 						},
+// 						{key: MalKeyword.create('symbol'), type: 'string'},
+// 						{key: MalKeyword.create('start'), type: 'number', default: 0},
+// 						{key: MalKeyword.create('duration'), type: 'number', default: 1},
+// 						{
+// 							key: MalKeyword.create('fps'),
+// 							label: 'FPS',
+// 							type: 'number',
+// 							default: 24,
+// 						},
+// 						{
+// 							key: MalKeyword.create('bounds'),
+// 							type: 'rect2d',
+// 							default: [0, 0, 100, 100],
+// 						},
+// 					],
+// 				},
+// 			],
+// 			'initial-params': [
+// 				MalKeyword.create('format'),
+// 				'gif',
+// 				MalKeyword.create('scaling'),
+// 				1,
+// 				MalKeyword.create('symbol'),
+// 				'time',
+// 				MalKeyword.create('start'),
+// 				0,
+// 				MalKeyword.create('duration'),
+// 				1,
+// 				MalKeyword.create('fps'),
+// 				24,
+// 				MalKeyword.create('bounds'),
+// 				[0, 0, 200, 200],
+// 			],
+// 		})
+// 	)
+// )
 
-ConsoleScope.defn(
-	'publish-gist',
-	setMeta(
-		(...args: MalVal[]) => {
-			const code = ConsoleScope.var('*sketch*').value as string
+// ConsoleScope.defn(
+// 	'publish-gist',
+// 	setMeta(
+// 		(...args: MalVal[]) => {
+// 			const code = ConsoleScope.var('*sketch*').value as string
 
-			// eslint-disable-next-line prefer-const
-			const {name, user, token} = MalMap.fromMalSeq(...args).toJS() as {
-				name: string
-				user: string
-				token: string
-			}
+// 			// eslint-disable-next-line prefer-const
+// 			const {name, user, token} = MalMap.fromMalSeq(...args).toJS() as {
+// 				name: string
+// 				user: string
+// 				token: string
+// 			}
 
-			if (typeof user !== 'string' || typeof token !== 'string') {
-				throw new MalError(`Parameters :user and :token must be specified.
-	Get the token from https://github.com/settings/tokens/new with 'gist' option turned on.`)
-			}
+// 			if (typeof user !== 'string' || typeof token !== 'string') {
+// 				throw new MalError(`Parameters :user and :token must be specified.
+// 	Get the token from https://github.com/settings/tokens/new with 'gist' option turned on.`)
+// 			}
 
-			localStorage.setItem('gist_api_token', JSON.stringify({user, token}))
+// 			localStorage.setItem('gist_api_token', JSON.stringify({user, token}))
 
-			const filename = generateFilename(name)
+// 			const filename = generateFilename(name)
 
-			async function publishToGist() {
-				const res = await fetch('https://api.github.com/gists', {
-					method: 'POST',
-					headers: {
-						Authorization: 'Basic ' + btoa(`${user}:${token}`),
-					},
-					body: JSON.stringify({
-						public: true,
-						files: {
-							[filename]: {
-								content: code,
-							},
-						},
-					}),
-				})
+// 			async function publishToGist() {
+// 				const res = await fetch('https://api.github.com/gists', {
+// 					method: 'POST',
+// 					headers: {
+// 						Authorization: 'Basic ' + btoa(`${user}:${token}`),
+// 					},
+// 					body: JSON.stringify({
+// 						public: true,
+// 						files: {
+// 							[filename]: {
+// 								content: code,
+// 							},
+// 						},
+// 					}),
+// 				})
 
-				if (res.ok) {
-					const data = await res.json()
+// 				if (res.ok) {
+// 					const data = await res.json()
 
-					const codeURL = data.files[filename].raw_url
-					generateSketchURL(codeURL)
-				} else {
-					printer.error('Invalid username or token')
-				}
-			}
+// 					const codeURL = data.files[filename].raw_url
+// 					generateSketchURL(codeURL)
+// 				} else {
+// 					printer.error('Invalid username or token')
+// 				}
+// 			}
 
-			publishToGist()
+// 			publishToGist()
 
-			printer.log(`Publishing to Gist... user=${user}, token=${token}`)
+// 			printer.log(`Publishing to Gist... user=${user}, token=${token}`)
 
-			return MalNil.create()
-		},
-		jsToMal({
-			doc:
-				'Publishes the current sketch to Gist then generates Code URL. Please set `user` to your GitHub username and `token` to a personal access token that you can generate from [Developer Settings](https://github.com/settings/tokens/new) with the **gist** option turned on.',
-			params: [
-				{
-					label: 'Name',
-					type: 'string',
-				},
-				{
-					type: 'map',
-					variadic: true,
-					items: [
-						{
-							key: MalKeyword.create('user'),
-							label: 'User',
-							type: 'string',
-						},
-						{
-							key: MalKeyword.create('token'),
-							label: 'Token',
-							type: 'string',
-						},
-					],
-				},
-			],
-			'initial-params': () => {
-				const sketchName = generateFilename()
+// 			return MalNil.create()
+// 		},
+// 		jsToMal({
+// 			doc:
+// 				'Publishes the current sketch to Gist then generates Code URL. Please set `user` to your GitHub username and `token` to a personal access token that you can generate from [Developer Settings](https://github.com/settings/tokens/new) with the **gist** option turned on.',
+// 			params: [
+// 				{
+// 					label: 'Name',
+// 					type: 'string',
+// 				},
+// 				{
+// 					type: 'map',
+// 					variadic: true,
+// 					items: [
+// 						{
+// 							key: MalKeyword.create('user'),
+// 							label: 'User',
+// 							type: 'string',
+// 						},
+// 						{
+// 							key: MalKeyword.create('token'),
+// 							label: 'Token',
+// 							type: 'string',
+// 						},
+// 					],
+// 				},
+// 			],
+// 			'initial-params': () => {
+// 				const sketchName = generateFilename()
 
-				let user = '',
-					token = ''
+// 				let user = '',
+// 					token = ''
 
-				const saved = localStorage.getItem('gist_api_token')
-				if (saved !== null) {
-					;({user, token} = JSON.parse(saved) as {
-						user: string
-						token: string
-					})
-				}
+// 				const saved = localStorage.getItem('gist_api_token')
+// 				if (saved !== null) {
+// 					;({user, token} = JSON.parse(saved) as {
+// 						user: string
+// 						token: string
+// 					})
+// 				}
 
-				return [
-					sketchName,
-					MalKeyword.create('user'),
-					user,
-					MalKeyword.create('token'),
-					token,
-				]
-			},
-		})
-	)
-)
+// 				return [
+// 					sketchName,
+// 					MalKeyword.create('user'),
+// 					user,
+// 					MalKeyword.create('token'),
+// 					token,
+// 				]
+// 			},
+// 		})
+// 	)
+// )
 
 ConsoleScope.defn('generate-embed-url', () => {
 	const sketch = ConsoleScope.var('*sketch*').value as string
