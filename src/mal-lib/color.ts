b1edbaab94229665a15990462d52913f2524982c
Baku Hashimoto <mail@baku89.com>
2020-10-09 11:37:28 +0900

    Fix color function typings

--- src/mal-lib/color.ts
+++ src/mal-lib/color.ts
@@ -11,44 +11,45 @@ import {
 const Exports = [
 	[
 		'color/mix',
-		function (
+		(
 			color1: MalString,
 			color2: MalString,
 			ratio: MalNumber,
 			mode = MalString.create('lrgb')
-		) {
-			return chroma
-				.mix(color1.value, color2.value, ratio.value, mode.value as any)
-				.css()
-		},
+		) =>
+			MalString.create(
+				chroma
+					.mix(color1.value, color2.value, ratio.value, mode.value as any)
+					.css()
+			),
 	],
 	[
 		'color/brighten',
-		function (color: MalString, value = MalNumber.create(1)) {
-			return chroma(color.value).brighten(value.value).hex()
-		},
+		(color: MalString, value = MalNumber.create(1)) =>
+			MalString.create(chroma(color.value).brighten(value.value).hex()),
 	],
 	[
 		'color/darken',
-		function (color: MalString, value = MalNumber.create(1)) {
-			return chroma(color.value).darken(value.value).hex()
-		},
+		(color: MalString, value = MalNumber.create(1)) =>
+			MalString.create(chroma(color.value).darken(value.value).hex()),
 	],
 	[
 		'color/invert',
-		function (color: MalString, mode = MalString.create('rgb')) {
+		(color: MalString, mode = MalString.create('rgb')) => {
 			const c = chroma(color.value)
 			const a = c.alpha()
 			if (mode.value === 'hsl') {
 				const [h, s, l] = c.hsl()
-				return chroma((h + 180) % 360, 1 - s, 1 - l, 'hsl').hex()
+				return MalString.create(
+					chroma((h + 180) % 360, 1 - s, 1 - l, 'hsl').hex()
+				)
 			} else {
 				const [r, g, b] = c.rgb()
-				return chroma([255 - r, 255 - g, 255 - b, a]).hex()
+				return MalString.create(chroma([255 - r, 255 - g, 255 - b, a]).hex())
 			}
 		},
 	],
-] as [string, Function][]
+] as [string, any][]
 
 const Exp = MalList.create(
 	MalSymbol.create('do'),
