86414cf4fe0a9f69d7e1da20cdd87acaa5ec1d8e
Baku Hashimoto <mail@baku89.com>
2020-10-12 22:52:52 +0900

    Make console page work

--- src/components/pages/PageInterpreter.vue
+++ src/components/pages/PageInterpreter.vue
@@ -1,17 +1,38 @@
 <template>
-	<div>This is Interpreter</div>
+	<div class="PageInterpreter">
+		<Console :scope="scope" @setup="onSetupConsole" />
+	</div>
 </template>
 
 <script lang="ts">
 import 'normalize.css'
-import {defineComponent} from 'vue'
+import Scope from '@/mal/scope'
+import {defineComponent, ref, shallowRef} from 'vue'
+import Console from '@/components//Console.vue'
+
+// replScope.REP(`(str "Glisp [" *host-language* "]")`)
 
 export default defineComponent({
 	name: 'PageInterpreter',
+	components: {
+		Console,
+	},
+	setup() {
+		const scope = shallowRef(new Scope())
+
+		function onSetupConsole() {
+			scope.value.REP(`(str "Glisp [" *host-language* "]")`)
+		}
+
+		return {scope, onSetupConsole}
+	},
 })
 </script>
 
 <style lang="stylus">
 @import '../style/common.styl'
 @import '../style/global.styl'
+
+.PageInterpreter
+	height 100vh
 </style>
