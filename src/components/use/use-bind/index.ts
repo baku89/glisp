0d2a795355296075a10e526056188dc347b4edf5
Baku Hashimoto <mail@baku89.com>
2020-10-13 22:55:30 +0900

    Add record-bind

--- src/components/use/use-bind/index.ts
+++ src/components/use/use-bind/index.ts
@@ -1,8 +1,10 @@
 import Mousetrap from 'mousetrap'
 import './mousetrap-global-bind'
+import './mousetrap-record'
 
 import Scope from '@/mal/scope'
 import {MalError, MalNil, MalString, MalVal} from '@/mal/types'
+import {printer} from '@/mal/printer'
 
 export default function useBind(scope: Scope) {
 	scope.def('set-bind', (command: MalVal, exp: MalVal) => {
@@ -28,6 +30,13 @@ export default function useBind(scope: Scope) {
 			Mousetrap.bindGlobal(cmd, callback)
 		}
 
+		return MalNil.create()
+	})
+
+	scope.def('record-bind', () => {
+		;(Mousetrap as any).record((seq: any) => {
+			printer.log('You pressed: ' + seq.join(' '))
+		})
 		return MalNil.create()
 	})
 }
