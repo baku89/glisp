1678119623f2730492ff71d52ac8ddf4b4af3402
Baku Hashimoto <mail@baku89.com>
2020-10-14 16:01:35 +0900

    Uncomment renderer

--- src/renderer/canvas-renderer/index.ts
+++ src/renderer/canvas-renderer/index.ts
@@ -1,29 +1,29 @@
-// import CanvasRenderer from './canvas-renderer'
-// import * as Comlink from 'comlink'
-// import {mat2d} from 'gl-matrix'
+import CanvasRenderer from './canvas-renderer'
+import * as Comlink from 'comlink'
+import {mat2d} from 'gl-matrix'
 
-// export interface ViewerSettings {
-// 	viewTransform?: mat2d
-// 	guideColor?: string
-// }
+export interface ViewerSettings {
+	viewTransform?: mat2d
+	guideColor?: string
+}
 
-// export type CanvasRendererType = CanvasRenderer | Comlink.Remote<CanvasRenderer>
+export type CanvasRendererType = CanvasRenderer | Comlink.Remote<CanvasRenderer>
 
 export default async function createCanvasRender(canvas?: HTMLCanvasElement) {
-	// let renderer: CanvasRendererType
-	// if (!canvas) {
-	// 	canvas = document.createElement('canvas')
-	// }
-	// if (typeof OffscreenCanvas !== 'undefined') {
-	// 	const CanvasRendererWorker = Comlink.wrap<CanvasRenderer>(
-	// 		new Worker('./worker.ts', {type: 'module'})
-	// 	) as any
-	// 	const offscreenCanvas = canvas.transferControlToOffscreen()
-	// 	renderer = (await new CanvasRendererWorker(
-	// 		Comlink.transfer(offscreenCanvas, [offscreenCanvas])
-	// 	)) as Comlink.Remote<CanvasRenderer>
-	// } else {
-	// 	renderer = new CanvasRenderer(canvas)
-	// }
-	// return renderer
+	let renderer: CanvasRendererType
+	if (!canvas) {
+		canvas = document.createElement('canvas')
+	}
+	if (typeof OffscreenCanvas !== 'undefined') {
+		const CanvasRendererWorker = Comlink.wrap<CanvasRenderer>(
+			new Worker('./worker.ts', {type: 'module'})
+		) as any
+		const offscreenCanvas = canvas.transferControlToOffscreen()
+		renderer = (await new CanvasRendererWorker(
+			Comlink.transfer(offscreenCanvas, [offscreenCanvas])
+		)) as Comlink.Remote<CanvasRenderer>
+	} else {
+		renderer = new CanvasRenderer(canvas)
+	}
+	return renderer
 }
