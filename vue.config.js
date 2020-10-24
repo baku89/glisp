134db1365183f427adbb89fa6a93297c11a5aee9
Baku Hashimoto <mail@baku89.com>
2020-10-24 18:06:24 +0900

    Add glslify loader

--- vue.config.js
+++ vue.config.js
@@ -73,6 +73,16 @@ module.exports = {
 				exposes: ['$', 'jQuery'],
 			})
 
+		// Glslify
+		config.module
+			.rule('glsl')
+			.test(/\.(glsl|vs|fs|vert|frag)$/)
+			.use('raw-loader')
+			.loader('raw-loader')
+			.end()
+			.use('glslify-loader')
+			.loader('glslify-loader')
+
 		// Copy logo.png to dist
 		config.plugin('copy-assets').use(CopyPlugin, [
 			{
