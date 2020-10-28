4e69858a19dd93a573e9470941c8333d372073dd
Baku Hashimoto <mail@baku89.com>
2020-10-28 11:04:09 +0900

    Split up Picker as another component

---
+++ src/components/inputs/InputColorPicker/index.ts
@@ -0,0 +1,1 @@
+export {default as InputColorPicker} from './InputColorPicker.vue'
---
+++ src/components/layouts/GlslCanvas.vue
@@ -0,0 +1,55 @@
+<template>
+	<canvas ref="canvasEl" />
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, onUnmounted, PropType, ref} from 'vue'
+import {Canvas} from 'glsl-canvas-js'
+
+interface UniformsProp {
+	[name: string]: number[] | string
+}
+
+export default defineComponent({
+	name: 'GlslCnavas',
+	props: {
+		fragmentString: {
+			type: String,
+			default: `
+				precision mediump float;
+				uniform vec2 u_resolution; 
+				void main() {
+					vec2 uv = gl_FragCoord.xy / u_resolution;
+					gl_FragColor = vec4(uv, 0.0, 1.0);
+				}`,
+		},
+		uniforms: {
+			type: Object as PropType<UniformsProp>,
+			default: () => ({}),
+		},
+	},
+	setup(props) {
+		const canvasEl = ref<null | HTMLCanvasElement>(null)
+
+		let glsl: any
+
+		onMounted(() => {
+			if (!canvasEl.value) return
+
+			const glsl = new Canvas(canvasEl.value, {
+				fragmentString: props.fragmentString,
+			})
+
+			glsl.setUniforms(props.uniforms)
+		})
+
+		onUnmounted(() => {
+			glsl?.destroy()
+		})
+
+		return {canvasEl}
+	},
+})
+</script>
+
+
