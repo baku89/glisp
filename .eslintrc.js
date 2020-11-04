5979f7bb59646ba2ec0fa28fe3a176b49756e662
Baku Hashimoto <mail@baku89.com>
2020-11-04 22:16:24 +0900

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,24 @@ module.exports = {
 		commonjs: true,
 	},
 	extends: [
-		'plugin:vue/vue3-essential',
 		'eslint:recommended',
+		'plugin:vue/vue3-essential',
 		'@vue/typescript/recommended',
 		'@vue/prettier',
 		'@vue/prettier/@typescript-eslint',
+		'plugin:import/typescript',
 	],
 	parserOptions: {
 		ecmaVersion: 2020,
 	},
+	plugins: ['simple-import-sort', 'unused-imports'],
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-use-before-define': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'simple-import-sort/sort': 'error',
+		'unused-imports/no-unused-imports-ts': 'error',
 	},
 }
