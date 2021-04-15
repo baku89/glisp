3f0272374871bb0e8dc5640c0ed14ff416b9715c
Baku Hashimoto <mail@baku89.com>
2021-04-15 17:34:10 +0900

    Update themes

--- src/components/use/use-theme/index.ts
+++ src/components/use/use-theme/index.ts
@@ -1,16 +1,18 @@
 import {ref} from '@vue/reactivity'
 import {computed, watchEffect} from '@vue/runtime-core'
 
+import LightOwl from './builtin-themes/Light Owl.json'
 import OceanDarkExtended from './builtin-themes/Ocean Dark Extended.json'
 
-type Theme = typeof OceanDarkExtended
+type Theme = typeof OceanDarkExtended & typeof LightOwl
 
 const ThemePresets = {
-	'Ocean Dark Extended': OceanDarkExtended,
+	'Ocean Dark Extended': OceanDarkExtended as Theme,
+	'Light Owl': LightOwl as Theme,
 } as {[name: string]: Theme}
 
 export function useTheme() {
-	const name = ref('Ocean Dark Extended')
+	const name = ref('Light Owl')
 
 	const theme = computed(() => ThemePresets[name.value])
 
@@ -19,11 +21,13 @@ export function useTheme() {
 		const htmlStyle = document.documentElement.style
 
 		for (const [name, color] of Object.entries(theme.value.colors)) {
+			if (typeof color !== 'string') continue
 			const varName = '--' + name.replaceAll('.', '-')
 			htmlStyle.setProperty(varName, color)
 		}
 
 		for (const token of theme.value.tokenColors) {
+			if (typeof token.scope !== 'string') continue
 			const varName = '--' + token.scope.replaceAll('.', '-')
 			htmlStyle.setProperty(varName, token.settings.foreground)
 		}
