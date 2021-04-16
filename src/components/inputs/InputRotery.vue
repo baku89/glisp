15dd3b9135dfc6c63e841bc554b654ba396f1878
Baku Hashimoto <mail@baku89.com>
2021-04-16 17:48:41 +0900

    Refine InputRotery design

--- src/components/inputs/InputRotery.vue
+++ src/components/inputs/InputRotery.vue
@@ -5,34 +5,56 @@
 		ref="el"
 		v-bind="$attrs"
 	>
-		<span
-			class="InputRotery__scale"
-			@mouseenter="tweakMode = 'absolute'"
-			@mouseleave="!tweaking ? (tweakMode = 'relative') : null"
-			:style="{
-				transform: `rotate(${modelValue}rad)`,
-			}"
-		/>
+		<svg class="InputRotery__rotery" viewBox="0 0 32 32">
+			<circle class="InputRotery__circle" cx="16" cy="16" r="16" />
+			<line
+				class="InputRotery__scale"
+				@mouseenter="tweakMode = 'absolute'"
+				@mouseleave="!tweaking ? (tweakMode = 'relative') : null"
+				:style="{
+					transform: `rotate(${modelValue}rad)`,
+				}"
+				stroke-
+				x1="20"
+				y1="16"
+				x2="32"
+				y2="16"
+			/>
+		</svg>
 	</button>
 	<teleport to="body">
-		<svg
-			v-if="tweaking"
-			class="InputRotery__overlay"
-			:style="{cursor: overlayCursor}"
-		>
-			<line
-				v-if="tweakMode === 'absolute'"
-				class="bold"
-				:x1="origin[0]"
-				:y1="origin[1]"
-				:x2="absolutePos[0]"
-				:y2="absolutePos[1]"
-			/>
-			<path v-if="tweakMode === 'relative'" class="bold" :d="overlayArcPath" />
-			<text class="label" :x="absolutePos[0] + 15" :y="absolutePos[1] - 10">
+		<template v-if="tweaking">
+			<svg class="InputRotery__overlay">
+				<line
+					v-if="tweakMode === 'absolute'"
+					class="bold"
+					:x1="overlayLineOrigin[0]"
+					:y1="overlayLineOrigin[1]"
+					:x2="absolutePos[0]"
+					:y2="absolutePos[1]"
+				/>
+				<path
+					v-if="tweakMode === 'relative'"
+					class="bold"
+					:d="overlayArcPath"
+				/>
+			</svg>
+			<div
+				class="InputRotery__overlay-label"
+				:style="{
+					top: absolutePos[1] + 'px',
+					left: absolutePos[0] + 'px',
+				}"
+			>
 				{{ overlayLabel }}
-			</text>
-		</svg>
+				<span
+					class="arrows"
+					:style="{
+						transform: `rotate(${overlayArrowAngle}rad)`,
+					}"
+				/>
+			</div>
+		</template>
 	</teleport>
 </template>
 
@@ -122,21 +144,29 @@ export default defineComponent({
 			return deg.toFixed(1) + '°'
 		})
 
-		const overlayCursor = computed(() => {
-			const angle = mod(
-				(Math.atan2(pos.value[1], pos.value[0]) / PI) * 180,
-				180
+		const overlayArrowAngle = computed(() => {
+			return (
+				Math.atan2(
+					absolutePos.value[1] - origin.value[1],
+					absolutePos.value[0] - origin.value[0]
+				) +
+				Math.PI / 2
 			)
+		})
 
-			if (angle < 22.5 || angle > 157.5) {
-				return 'ns-resize'
-			} else if (angle < 67.5) {
-				return 'nesw-resize'
-			} else if (angle < 112.5) {
-				return 'ew-resize'
-			} else {
-				return 'nwse-resize'
-			}
+		const overlayLineOrigin = computed(() => {
+			const o = origin.value
+			const t = absolutePos.value
+			const radius = 10
+
+			const p = vec2.create()
+
+			vec2.sub(p, t, o)
+			vec2.normalize(p, p)
+			vec2.scale(p, p, radius)
+			vec2.add(p, o, p)
+
+			return p
 		})
 
 		const overlayArcPath = computed(() => {
@@ -153,7 +183,7 @@ export default defineComponent({
 			const turns =
 				Math.floor(Math.abs(end - start) / PI_2) * Math.sign(end - start)
 
-			const center = origin.value
+			const c = origin.value
 
 			// Create arc
 			const arcRadius = baseRadius + turns * radiusStep
@@ -164,8 +194,8 @@ export default defineComponent({
 			const startInTurn = mod(start, PI_2)
 			const endInTurn = startInTurn + offsetInTurn
 
-			const from = addDirectionVector(center, startInTurn, arcRadius)
-			const to = addDirectionVector(center, endInTurn, arcRadius)
+			const from = addDirectionVector(c, startInTurn, arcRadius)
+			const to = addDirectionVector(c, endInTurn, arcRadius)
 
 			const angleBetween = Math.abs(startInTurn - endInTurn)
 
@@ -182,8 +212,8 @@ export default defineComponent({
 			let circles = ''
 			for (let i = 0, step = Math.sign(turns); i !== turns; i += step) {
 				const radius = baseRadius + i * radiusStep
-				const right = `${center[0] + radius} ${center[1]}`
-				const left = `${center[0] - radius} ${center[1]}`
+				const right = `${c[0] + radius} ${c[1]}`
+				const left = `${c[0] - radius} ${c[1]}`
 				circles += `M ${right}
 										A ${radius} ${radius} 0 1 0 ${left}
 										A ${radius} ${radius} 0 1 0 ${right}`
@@ -200,10 +230,10 @@ export default defineComponent({
 
 			// overlay
 			absolutePos,
-			origin,
+			overlayLineOrigin,
 			overlayArcPath,
 			overlayLabel,
-			overlayCursor,
+			overlayArrowAngle,
 		}
 	},
 	inheritAttrs: false,
@@ -218,63 +248,72 @@ export default defineComponent({
 	overflow hidden
 	padding 0
 	width $button-height
-	border-radius 50%
 	transition all 0.1s cubic-bezier(0.25, 0.1, 0, 1)
 	aspect-ratio 1
 
-	// background
-	&:before
-		position absolute
-		top 0
-		left 0
-		display block
+	&:hover, &.tweaking
+		z-index 1
+
+	&__rotery
 		width 100%
 		height 100%
-		background var(--base04)
-		content ''
 
 	// Enlarge
 	&:hover, &.tweaking
 		transform scale(3)
 
-		&:before
-			opacity 0.8
+	&__circle
+		fill var(--base04)
+
+		&:hover
+			fill var(--highlight)
 
 	&__scale
-		position absolute
-		top 30%
-		left 50%
-		display block
-		width 52%
-		height 40%
-		border-radius 50% 0 0 50%
-		transform-origin 0 50%
+		transform-origin 16px 16px
+		stroke var(--base00)
+		stroke-width 3
+		stroke-linecap round
 
-		&:before
-			position absolute
-			top 50%
-			left 0
-			display block
-			width 100%
-			height 1px
-			background var(--base00)
-			content ''
-			transform translateY(-70%)
-			pointer-events none
-
-			~/.tweak-absolute &
-				background var(--highlight) !important
-
-	&:hover, &:focus, &.tweaking
-		&:before
-			background var(--highlight)
-
-		~/__scale:before
-			background var(--base00)
-
-	&.tweak-absolute:before
-		background var(--base04) !important
+		~/.tweak-absolute &
+			stroke var(--highlight)
 
 	&__overlay
 		input-overlay()
+
+	&__overlay-label
+		z-index 1001
+		cursor none
+		font-monospace()
+		position fixed
+		padding 0.4em
+		border-radius 4px
+		background var(--translucent)
+		color var(--highlight)
+		font-weight bold
+		transition opacity 0.2s ease
+		transform translate(-50%, -50%)
+
+		.arrows
+			position absolute
+			top 0
+			left 0
+			width 100%
+			height 100%
+
+			&:before, &:after
+				position absolute
+				top 50%
+				display block
+				width 1em
+				text-align center
+				font-weight normal
+				transform translateY(-50%)
+
+			&:before
+				right 100%
+				content '<'
+
+			&:after
+				left 100%
+				content '>'
 </style>
