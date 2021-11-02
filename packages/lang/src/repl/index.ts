97ff62dbe289b7be3a23c3ad2fd44366404cfc04
Baku Hashimoto <mail@baku89.com>
2021-11-02 20:30:14 +0900

    Print logs

--- packages/lang/src/repl/index.ts
+++ packages/lang/src/repl/index.ts
@@ -1,6 +1,7 @@
 import * as os from 'os'
 import * as repl from 'repl'
 
+import {ValueWithLog} from '../exp'
 import {parse} from '../parser'
 
 function startRepl() {
@@ -8,9 +9,20 @@ function startRepl() {
 		prompt: '>> ',
 		eval(input, context, file, cb) {
 			const exp = parse(input)
-			cb(null, exp.eval().result.print())
+			const result = exp.eval()
+			cb(null, result)
 		},
-		writer: v => v,
+		writer: ({result, log}: ValueWithLog) => {
+			let str = ''
+
+			if (log.length > 0) {
+				str += log.map(l => `[${l.level}] ${l.reason}\n`).join('')
+			}
+
+			str += result.print()
+
+			return str
+		},
 	})
 }
 
