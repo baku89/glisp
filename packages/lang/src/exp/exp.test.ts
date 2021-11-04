89c38a91c3a0c86b3bb791a10c6d5c41ab25b1d3
Baku Hashimoto <mail@baku89.com>
2021-11-04 12:08:21 +0900

    Add some mores tests on polymorphic type inferrence

--- packages/lang/src/exp/exp.test.ts
+++ packages/lang/src/exp/exp.test.ts
@@ -1,4 +1,5 @@
 import * as Exp from '../exp'
+import * as Parser from '../parser'
 import {GlobalScope} from '../std/global'
 import * as Val from '../val'
 
@@ -36,25 +37,29 @@ describe('infer type', () => {
 })
 
 describe('infer polymorphic function application', () => {
-	testInfer(Exp.call(Exp.sym('identity'), Exp.int(1)), Val.int(1))
-	testInfer(
-		Exp.call(Exp.sym('if'), Exp.bool(true), Exp.int(1), Exp.int(2)),
-		Val.uniteTy(Val.int(1), Val.int(2))
-	)
-	testInfer(
-		Exp.call(
-			Exp.sym('identity'),
-			Exp.call(Exp.sym('+'), Exp.int(1), Exp.int(2))
-		),
-		Val.tyInt
-	)
+	testInfer('(identity 1)', Val.int(1))
+	testInfer('(if true 1 2)', Val.uniteTy(Val.int(1), Val.int(2)))
+	testInfer('(identity (+ 1 2))', Val.tyInt)
+	testInfer('(+ (identity 1) (identity 2))', Val.tyInt)
+	testInfer('(if (identity true) (identity 2) (* 1 2))', Val.tyInt)
+	testInfer('(identity (identity 1))', Val.int(1))
 })
 
-function testInfer(input: Exp.Node, expected: Val.Value) {
-	test(`${input.print()} is inferred to be ${expected.print()}`, () => {
-		input.parent = GlobalScope
+function parse(input: string | Exp.Node): Exp.Node {
+	if (typeof input === 'string') {
+		return Parser.parse(input)
+	} else {
+		return input
+	}
+}
 
-		const inferred = input.infer().result
+function testInfer(input: string | Exp.Node, expected: Val.Value) {
+	const exp = parse(input)
+
+	test(`${exp.print()} is inferred to be ${expected.print()}`, () => {
+		exp.parent = GlobalScope
+
+		const inferred = exp.infer().result
 		const equal = inferred.isEqualTo(expected)
 		if (!equal) throw new Error('Got=' + inferred.print())
 	})
