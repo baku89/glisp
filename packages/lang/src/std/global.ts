46a521c12c8341bae49022a0bedf9730f3b405bb
Baku Hashimoto <mail@baku89.com>
2021-11-05 17:16:38 +0900

    Refactor global scope declarations

--- packages/lang/src/std/global.ts
+++ packages/lang/src/std/global.ts
@@ -7,79 +7,67 @@ const T = Val.tyVar()
 const U = Val.tyVar()
 const V = Val.tyVar()
 
+function defn(
+	value: Val.Fn['value'],
+	param: Record<string, Val.Value>,
+	out: Val.Value
+) {
+	return obj(Val.fn(value, param, out))
+}
+
 export const GlobalScope = scope({
 	_: obj(Val.bottom),
 	Int: obj(Val.tyInt),
 	Bool: obj(Val.tyBool),
-	succ: obj(
-		Val.fn((x: Val.Int) => Val.int(x.value + 1), {x: Val.tyInt}, Val.tyInt)
+	succ: defn((x: Val.Int) => Val.int(x.value + 1), {x: Val.tyInt}, Val.tyInt),
+	'even?': defn(
+		(x: Val.Int) => Val.bool(x.value % 2 === 0),
+		{x: Val.tyInt},
+		Val.tyBool
 	),
-	'even?': obj(
-		Val.fn(
-			(x: Val.Int) => Val.bool(x.value % 2 === 0),
-			{x: Val.tyInt},
-			Val.tyBool
-		)
+	'+': defn(
+		(a: Val.Int, b: Val.Int) => Val.int(a.value + b.value),
+		{x: Val.tyInt, y: Val.tyInt},
+		Val.tyInt
 	),
-	'+': obj(
-		Val.fn(
-			(a: Val.Int, b: Val.Int) => Val.int(a.value + b.value),
-			{x: Val.tyInt, y: Val.tyInt},
-			Val.tyInt
-		)
+	'*': defn(
+		(a: Val.Int, b: Val.Int) => Val.int(a.value * b.value),
+		{x: Val.tyInt, y: Val.tyInt},
+		Val.tyInt
 	),
-	'*': obj(
-		Val.fn(
-			(a: Val.Int, b: Val.Int) => Val.int(a.value * b.value),
-			{x: Val.tyInt, y: Val.tyInt},
-			Val.tyInt
-		)
+	'<': defn(
+		(a: Val.Int, b: Val.Int) => Val.bool(a.value < b.value),
+		{x: Val.tyInt, y: Val.tyInt},
+		Val.tyBool
 	),
-	'<': obj(
-		Val.fn(
-			(a: Val.Int, b: Val.Int) => Val.bool(a.value < b.value),
-			{x: Val.tyInt, y: Val.tyInt},
-			Val.tyBool
-		)
-	),
-	not: obj(
-		Val.fn((x: Val.Bool) => Val.bool(!x.value), {x: Val.tyBool}, Val.tyBool)
-	),
-	'|': obj(
-		Val.fn(
-			(t1: Val.Value, t2: Val.Value) => Val.uniteTy(t1, t2),
-			{x: Val.tyInt, y: Val.tyInt},
-			Val.tyInt
-		)
+	not: defn((x: Val.Bool) => Val.bool(!x.value), {x: Val.tyBool}, Val.tyBool),
+	'|': defn(
+		(t1: Val.Value, t2: Val.Value) => Val.uniteTy(t1, t2),
+		{x: Val.tyInt, y: Val.tyInt},
+		Val.tyInt
 	),
 	identity: obj(Val.fn((x: Val.Value) => x, {x: T}, T)),
-	if: obj(
-		Val.fn(
-			(test: Val.Bool, then: Val.Value, _else: Val.Value) => {
-				return test.value ? then : _else
-			},
-			{test: Val.tyBool, then: T, else: T},
-			T
-		)
+	if: defn(
+		(test: Val.Bool, then: Val.Value, _else: Val.Value) => {
+			return test.value ? then : _else
+		},
+		{test: Val.tyBool, then: T, else: T},
+		T
 	),
-	'.': obj(
-		Val.fn(
-			(f: Val.Fn, g: Val.Fn) => {
-				const name = keys(f.param)[0]
-				return Val.fn((x: Val.Value) => g.value(f.value(x)), {[name]: T}, V)
-			},
-			{f: Val.tyFn(T, U), g: Val.tyFn(U, V)},
-			Val.tyFn(T, V)
-		)
+	'.': defn(
+		(f: Val.Fn, g: Val.Fn) => {
+			const name = keys(f.param)[0]
+			return Val.fn((x: Val.Value) => g.value(f.value(x)), {[name]: T}, V)
+		},
+		{f: Val.tyFn(T, U), g: Val.tyFn(U, V)},
+		Val.tyFn(T, V)
 	),
-	twice: obj(
-		Val.fn(
-			(f: Val.Fn) => {
-				const name = keys(f.param)[0]
-				return Val.fn((x: Val.Value) => f.value(f.value(x)), {[name]: T}, T)
-			},
-			{f: Val.tyFn(T, T)},
-			Val.tyFn(T, T)
-		)
+	twice: defn(
+		(f: Val.Fn) => {
+			const name = keys(f.param)[0]
+			return Val.fn((x: Val.Value) => f.value(f.value(x)), {[name]: T}, T)
+		},
+		{f: Val.tyFn(T, T)},
+		Val.tyFn(T, T)
 	),
 })
