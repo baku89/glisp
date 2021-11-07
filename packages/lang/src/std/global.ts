4903f54de37290c6ffb387abc49debdb3bfa372a
Baku Hashimoto <mail@baku89.com>
2021-11-07 18:57:15 +0900

    Make `|` polymorphic

--- packages/lang/src/std/global.ts
+++ packages/lang/src/std/global.ts
@@ -45,8 +45,8 @@ export const GlobalScope = scope({
 	not: defn((x: Val.Bool) => Val.bool(!x.value), {x: Val.tyBool}, Val.tyBool),
 	'|': defn(
 		(t1: Val.Value, t2: Val.Value) => Val.uniteTy(t1, t2),
-		{x: Val.tyInt, y: Val.tyInt},
-		Val.tyInt
+		{x: T, y: T},
+		T
 	),
 	id: obj(Val.fn((x: Val.Value) => x, {x: T}, T)),
 	if: defn(
