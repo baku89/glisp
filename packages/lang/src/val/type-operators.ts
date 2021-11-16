205ee250375ee9f4fe11bcbb4a310fdb85a5cabb
Baku Hashimoto <mail@baku89.com>
2021-11-16 14:57:01 +0900

    Unify AlgCtor into TyVariant correctly

--- packages/lang/src/val/type-operators.ts
+++ packages/lang/src/val/type-operators.ts
@@ -7,18 +7,38 @@ export function uniteTy(...types: Value[]) {
 
 	const normalizedTypes = flattenedTypes.reduce((prev, ty) => {
 		const index = prev.findIndex(p => p.isSubtypeOf(ty))
-		if (index !== -1) {
+
+		const includesPrev = index !== -1
+		if (includesPrev) {
 			const cur = [...prev]
 			cur[index] = ty
 			return cur
 		}
 
-		const included = prev.some(p => ty.isSubtypeOf(p))
-		return included ? prev : [...prev, ty]
+		const includedByPrev = prev.some(p => ty.isSubtypeOf(p))
+		if (includedByPrev) {
+			return prev
+		}
+
+		if (ty.type === 'algCtor') {
+			const variant = ty.superType
+			const arr = [...prev, ty]
+			const indices = new Set(
+				variant.types.map(t => arr.findIndex(a => t.isSubtypeOf(a)))
+			)
+			const includesAllAlgCtors = !indices.has(-1)
+			if (includesAllAlgCtors) {
+				const arrSpliced = arr.filter((_, i) => !indices.has(i))
+				return [...arrSpliced, variant]
+			}
+		}
+
+		return [...prev, ty]
 	}, [] as Exclude<Value, TyUnion>[])
 
 	if (normalizedTypes.length === 0) return Bottom.instance
 	if (normalizedTypes.length === 1) return normalizedTypes[0]
+
 	return TyUnion.fromTypesUnsafe(normalizedTypes)
 }
 
