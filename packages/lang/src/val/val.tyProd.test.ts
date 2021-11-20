ba5e951d427c81d3d6816296fc2d0da3e49080dc
Baku Hashimoto <mail@baku89.com>
2021-11-20 12:39:00 +0900

    Add some more tests for TyProd

--- packages/lang/src/val/val.tyProd.test.ts
+++ packages/lang/src/val/val.tyProd.test.ts
@@ -8,3 +8,19 @@ test('default type of Vec2 to be (Vec2 0 0)', () => {
 		throw new Error('Got=' + Vec2.defaultValue.print())
 	}
 })
+
+test('(Vec2 10 10) equals to (Vec2 10 10)', () => {
+	const a = Vec2.fn(int(10), int(10)).result
+	const b = Vec2.fn(int(10), int(10)).result
+
+	expect(a.isEqualTo(b)).toBe(true)
+})
+
+test('Vec2 is a subtype of itself', () => {
+	expect(Vec2.isSubtypeOf(Vec2)).toBe(true)
+})
+
+test('(Vec2 10 10) is a subtype of Vec2', () => {
+	const a = Vec2.fn(int(10), int(10)).result
+	expect(a.isSubtypeOf(Vec2)).toBe(true)
+})
