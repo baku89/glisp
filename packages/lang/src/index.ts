183006065e2c8615d627a79f7c96b72df21f052f
Baku Hashimoto <mail@baku89.com>
2021-11-20 19:33:11 +0900

    Edit exports of the whole module

--- packages/lang/src/index.ts
+++ packages/lang/src/index.ts
@@ -1,3 +1,6 @@
-export function main() {
-	console.log('Hello, Glisp!')
-}
+import * as Exp from './exp'
+import {parse} from './parser'
+import {GlobalScope} from './std/global'
+import * as Val from './val'
+
+export {Val, Exp, GlobalScope, parse}
