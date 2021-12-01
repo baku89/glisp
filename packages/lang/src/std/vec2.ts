641e228a43781133cbeb9d9488e83e385da38433
Baku Hashimoto <mail@baku89.com>
2021-12-02 08:56:27 +0900

    Declare Vec2 function with Glisp

--- packages/lang/src/std/vec2.ts
+++ packages/lang/src/std/vec2.ts
@@ -1,5 +1,6 @@
 import * as Exp from '../exp'
-import {Writer} from '../utils/Writer'
+import {parse} from '../parser'
+import {GlobalScope} from '../std/global'
 import * as Val from '../val'
 
 const Vec2 = Val.tyProd('Vec2', {x: Val.tyNum, y: Val.tyNum}, [
@@ -7,34 +8,19 @@ const Vec2 = Val.tyProd('Vec2', {x: Val.tyNum, y: Val.tyNum}, [
 	Val.num(0),
 ])
 
-const scope = Exp.scope({
+const scope = GlobalScope.extend({
 	Vec2: Exp.obj(Vec2),
-	'Vec2/length': Exp.obj(
-		Val.fn(
-			(v: Val.Prod) => {
-				const x = (v.items[0] as Val.Num).value
-				const y = (v.items[1] as Val.Num).value
-				return Writer.of(Val.num(Math.sqrt(x * x + y * y)))
-			},
-			{x: Vec2},
-			Val.tyNum
-		)
-	),
-	'Vec2/+': Exp.obj(
-		Val.fn(
-			(x: Val.Prod, y: Val.Prod) => {
-				const [x1, y1] = x.items as Val.Num[]
-				const [x2, y2] = y.items as Val.Num[]
-
-				const sx = x1.value + x2.value
-				const sy = y1.value + y2.value
-
-				return Writer.of(Vec2.of(Val.num(sx), Val.num(sy)))
-			},
-			{x: Vec2, y: Vec2},
-			Vec2
-		)
-	),
 })
+
+scope.def(
+	'Vec2/length',
+	parse('(=> v:Vec2 {x = (v "x") y = (v "y") (sqrt (+ (* x x) (* y y)))})')
+)
+
+scope.def(
+	'Vec2/+',
+	parse(`(=> (x:Vec2 y:Vec2)
+	           (Vec2 (+ (x "x") (y "x")) (+ (x "y") (y "y"))))`)
+)
 
 export default scope
