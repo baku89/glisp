8a0f5cc8cc306fcb4c670b0dc41c61e7fc3a7329
Baku Hashimoto <mail@baku89.com>
2021-12-01 15:35:09 +0900

    Declare bindMaybe

--- packages/lang/src/std/global.ts
+++ packages/lang/src/std/global.ts
@@ -155,4 +155,22 @@ export const GlobalScope = scope({
 		{s: Val.all, t: Val.all},
 		Val.tyBool
 	),
+	bindMaybe: defn(
+		(f: Val.Fn, g: Val.Fn) =>
+			Val.fn(
+				(x: Val.Value) => {
+					const [fx, fLog] = f.fn(x).asTuple
+					if (fx.type === 'unit') return Writer.of(fx, ...fLog)
+					const [gx, gLog] = g.fn(fx).asTuple
+					return Writer.of(gx, ...fLog, ...gLog)
+				},
+				{x: T},
+				Val.uniteTy(Val.unit, V)
+			),
+		{
+			f: Val.tyFn(T, Val.uniteTy(Val.unit, U)),
+			g: Val.tyFn(U, Val.uniteTy(Val.unit, V)),
+		},
+		Val.tyFn(T, Val.uniteTy(Val.unit, V))
+	),
 })
