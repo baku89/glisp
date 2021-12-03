741188981ccb2fd8ba8e153f75a31606d02e06f7
Baku Hashimoto <mail@baku89.com>
2021-12-03 15:41:56 +0900

    Add a package demo-event

---
+++ packages/demo-event/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<p>Hello World</p>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+	name: 'App',
+})
+</script>
+
+<style lang="stylus">
+#app
+	color #2c3e50
+</style>
