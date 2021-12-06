d4dd5fa0136bcd73961031ff04b34d617c068bb3
Baku Hashimoto <mail@baku89.com>
2021-12-06 13:30:13 +0900

    Refactor test code

--- packages/lang/src/exp/exp2.test.ts
+++ packages/lang/src/exp/exp2.test.ts
@@ -1,57 +1,74 @@
-import * as Exp from '../exp'
+import {
+	all,
+	bottom,
+	dict,
+	False,
+	num,
+	str,
+	sym,
+	True,
+	tyBool,
+	tyDict,
+	tyFn,
+	tyNum,
+	tyStr,
+	tyValue,
+	tyVar,
+	tyVec,
+	unit,
+	Value,
+	vec,
+} from '../exp'
 import {parse, testEval} from '../utils/testUtils2'
 
 describe('evaluating literals', () => {
-	testEval(Exp.all, Exp.all)
-	testEval(Exp.bottom, Exp.bottom)
-	testEval(Exp.unit, Exp.unit)
-	testEval(Exp.num(0), Exp.num(0))
-	testEval(Exp.str('foo'), Exp.str('foo'))
-	testEval(Exp.sym('true2'), Exp.True)
-	testEval(Exp.sym('false2'), Exp.False)
+	testEval(all, all)
+	testEval(bottom, bottom)
+	testEval(unit, unit)
+	testEval(num(0), num(0))
+	testEval(str('foo'), str('foo'))
+	testEval(sym('true2'), True)
+	testEval(sym('false2'), False)
 
-	testEval('[]', Exp.vec())
-	testEval('[0]', Exp.vec(Exp.num(0)))
-	testEval('[...Num2]', Exp.tyVec([], Exp.tyNum))
-	testEval('[1 ...Num2]', Exp.tyVec([Exp.num(1)], Exp.tyNum))
-	testEval(Exp.vec(Exp.num(0)), Exp.vec(Exp.num(0)))
-	testEval('{a:1 b:2}', Exp.dict({a: Exp.num(1), b: Exp.num(2)}))
+	testEval('[]', vec())
+	testEval('[0]', vec(num(0)))
+	testEval('[...Num2]', tyVec([], tyNum))
+	testEval('[1 ...Num2]', tyVec([num(1)], tyNum))
+	testEval(vec(num(0)), vec(num(0)))
+	testEval('{a:1 b:2}', dict({a: num(1), b: num(2)}))
 	testEval(
 		'{a?:Num2 ...Str}',
-		Exp.tyDict({a: {optional: true, value: Exp.tyNum}}, Exp.tyStr)
+		tyDict({a: {optional: true, value: tyNum}}, tyStr)
 	)
-	testEval('(-> [Num2] Num2)', Exp.tyFn(Exp.tyNum, Exp.tyNum))
-	testEval('<T>', Exp.tyVar('T'))
-	testEval('{a = 10 a}', Exp.num(10))
-	testEval('{a = {a = 20 a} a}', Exp.num(20))
+	testEval('(-> [Num2] Num2)', tyFn(tyNum, tyNum))
+	testEval('<T>', tyVar('T'))
+	testEval('{a = 10 a}', num(10))
+	testEval('{a = {a = 20 a} a}', num(20))
 })
 
 describe('inferring expression type', () => {
-	testInfer('_', Exp.all)
-	testInfer('_|_', Exp.tyValue(Exp.bottom))
-	testInfer('()', Exp.unit)
-	testInfer('0', Exp.num(0))
-	testInfer('"foo"', Exp.str('foo'))
-	testInfer('true2', Exp.True)
+	testInfer('_', all)
+	testInfer('_|_', tyValue(bottom))
+	testInfer('()', unit)
+	testInfer('0', num(0))
+	testInfer('"foo"', str('foo'))
+	testInfer('true2', True)
 
-	testInfer('Num2', Exp.tyValue(Exp.tyNum))
-	testInfer('Bool2', Exp.tyValue(Exp.tyBool))
+	testInfer('Num2', tyValue(tyNum))
+	testInfer('Bool2', tyValue(tyBool))
 
-	testInfer('[]', Exp.vec())
-	testInfer('[0 1]', Exp.vec(Exp.num(0), Exp.num(1)))
-	testInfer('[Num2]', Exp.vec(Exp.tyValue(Exp.tyNum)))
-	testInfer('[...0]', Exp.tyValue(Exp.tyVec([], Exp.num(0))))
-	testInfer('{}', Exp.dict({}))
-	testInfer('{a:0}', Exp.dict({a: Exp.num(0)}))
-	testInfer('{a:Num2}', Exp.dict({a: Exp.tyValue(Exp.tyNum)}))
-	testInfer(
-		'{a?:0}',
-		Exp.tyValue(Exp.tyDict({a: {optional: true, value: Exp.num(0)}}))
-	)
-	testInfer('{...0}', Exp.tyValue(Exp.tyDict({}, Exp.num(0))))
-	testInfer('{a = Num2 a}', Exp.tyValue(Exp.tyNum))
+	testInfer('[]', vec())
+	testInfer('[0 1]', vec(num(0), num(1)))
+	testInfer('[Num2]', vec(tyValue(tyNum)))
+	testInfer('[...0]', tyValue(tyVec([], num(0))))
+	testInfer('{}', dict({}))
+	testInfer('{a:0}', dict({a: num(0)}))
+	testInfer('{a:Num2}', dict({a: tyValue(tyNum)}))
+	testInfer('{a?:0}', tyValue(tyDict({a: {optional: true, value: num(0)}})))
+	testInfer('{...0}', tyValue(tyDict({}, num(0))))
+	testInfer('{a = Num2 a}', tyValue(tyNum))
 
-	function testInfer(input: string, expected: Exp.Value) {
+	function testInfer(input: string, expected: Value) {
 		it(`${input} is inferred to be ${expected.print()}`, () => {
 			const inferred = parse(input).infer2()
 
