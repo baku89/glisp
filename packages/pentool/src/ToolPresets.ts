d264cf1ca383b7a9f24c292b64721f615df2dc2d
Baku Hashimoto <mail@baku89.com>
2021-12-15 17:11:14 +0900

    Add ToolPresets

---
+++ packages/pentool/src/ToolPresets.ts
@@ -0,0 +1,24 @@
+import TemplateText from 'raw-loader!./tools-default/template.js'
+import uid from 'uid'
+
+import Tool from './tool'
+
+export const presets = [
+	// fs.readFileSync(__dirname + '/tools-default/pencil.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/rectangle.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/centered-circle.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/bezier.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/spray.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/graph.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/triangle-strip.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/arc-strip.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/radial-line.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/dubins-path.js', 'utf-8'),
+	// fs.readFileSync(__dirname + '/tools-default/concentric-circles.js', 'utf-8'),
+].map(Tool.parseToolText)
+
+const template = Tool.parseToolText(TemplateText)
+
+export function createNew() {
+	return {...template, id: uid(10)}
+}
