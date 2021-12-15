5276fe7fc65a99ded612054e29baec1ed58a3872
Baku Hashimoto <mail@baku89.com>
2021-12-15 14:20:24 +0900

    Add pentool package

---
+++ packages/pentool/src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+HelloWorld(msg='Welcome to Your Vue.js + TypeScript App')
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		HelloWorld,
+	},
+})
+</script>
+
+<style lang="stylus">
+#app
+	margin-top 60px
+	color #2c3e50
+	text-align center
+	font-family Avenir, Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+</style>
