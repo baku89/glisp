8b5f99c430819a341b7537e7ef2843a365e2207a
Baku Hashimoto <mail@baku89.com>
2021-12-23 15:22:37 +0900

    Move Jest's configs to a separated file from package.json

---
+++ packages/lang/jest.config.ts
@@ -0,0 +1,24 @@
+import type {Config} from '@jest/types'
+
+const config: Config.InitialOptions = {
+	silent: false,
+	preset: 'ts-jest',
+	passWithNoTests: true,
+	projects: [
+		{
+			transform: {
+				'^.+\\.(t|j)s$': 'ts-jest',
+			},
+		},
+		{
+			displayName: 'lint',
+			runner: 'jest-runner-eslint',
+			testMatch: ['<rootDir>/src/**/*.ts'],
+			transform: {
+				'^.+\\.(t|j)s$': 'ts-jest',
+			},
+		},
+	],
+}
+
+export default config
