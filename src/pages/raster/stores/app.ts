3cf810819cc3132a140d66de4af6941f583440eb
Baku Hashimoto <mail@baku89.com>
2022-03-08 03:04:31 +0900

    Fix for moble

--- src/pages/raster/stores/app.ts
+++ src/pages/raster/stores/app.ts
@@ -8,7 +8,7 @@ export function useModuleApp(): StoreModule {
 				label: 'Reload App',
 				icon: '<path d="M29 16 C29 22 24 29 16 29 8 29 3 22 3 16 3 10 8 3 16 3 21 3 25 6 27 9 M20 10 L27 9 28 2" />',
 				exec() {
-					window.location.reload()
+					;(window.location.reload as any)(true)
 				},
 			},
 		},
